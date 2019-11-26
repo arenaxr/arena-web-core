432945dce875081c4708582c482759ce32d7c4a0
Ivan <r33t@cmu.edu>
2019-11-26 10:56:04 -0500

    Remove old arena-xr

--- arena-xr/camcleanup.sh
+++
@@ -1,1 +0,0 @@
-../camcleanup.sh
--- arena-xr/src/index.js
+++
@@ -1,8 +0,0 @@
-// require('three.xr.js');
-
-require('./systems/xr.js');
-
-require('./components/ar-mode-ui.js');
-require('./components/xr.js');
-require('./components/anchor.js');
-require('./components/reticle.js');
