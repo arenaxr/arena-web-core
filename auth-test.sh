70de7187eca9cfbdb02157aa67c03c350824d1a9
Michael Farb <mwfarb@andrew.cmu.edu>
2020-08-09 18:20:00 -0400

    test mospub with auth server in bash

---
+++ auth-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# example:
+#  ./auth-test.sh conix
+
+function get_token
+{
+  \curl -s 'http://xr.andrew.cmu.edu:8888'  2>&1
+}
+
+TOKEN=$(get_token)
+USER=$1
+
+# publish cube using token
+echo "calling mosquitto_pub, user: $USER, token: $TOKEN"
+mosquitto_pub -h oz.andrew.cmu.edu -t realm/s/auth-test/cube_1 -m '{"object_id" : "cube_1", "action": "create", "type": "object", "data": {"object_type": "cube", "position": {"x": 1, "y": 1, "z": -1}, "rotation": {"x": 0, "y": 0, "z": 0, "w": 1}, "scale": {"x": 1, "y": 1, "z": 1}, "color": "#FF0000"}}' -p 1884 -u $USER -P $TOKEN
