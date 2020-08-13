13ea6f780ecfafdad290a8db0fb1d6e3133b9362
Michael Farb <mwfarb@andrew.cmu.edu>
2020-08-12 23:30:27 -0400

    adding jose gen jwt for test cases per scene

--- auth-test/index.js
+++ auth-test/index.js
@@ -2,15 +2,26 @@ const config = require('./config.json');
 const express = require('express')
 const https = require('https')
 const fs = require('fs')
-const jose = require('jose')
-const { JWK, JWT, errors } = jose
+const { JWT, JWK } = require('jose')
 const app = express()
 const port = 8888
 
 const key = fs.readFileSync(config.keypath);
 const cert = fs.readFileSync(config.certpath);
+const jwk = JWK.asKey({ kty: 'oct', k: config.secret });
 
 const server = https.createServer({ key: key, cert: cert }, app);
+
+function generateToken(user = null, exp = '1 hour', sub = null, pub = null) {
+    claims = { "sub": user };
+    if (sub) {
+        claims.subs = sub;
+    }
+    if (pub) {
+        claims.publ = pub;
+    }
+    return JWT.sign(claims, jwk, { "alg": "HS256", "expiresIn": exp });
+}
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -19,15 +30,35 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', (req, res) => {
+    scene = req.query.scene
+    sobj = "realm/s/" + scene + "/#";
+    sadm = "realm/admin/s/" + scene + "/#";
     switch (req.query.username) {
-        case 'conix':
-            var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjb25peCIsImlhdCI6MTU5Njg3NDA4OCwiZXhwIjoxNjI4NDEwMDg4fQ.6Z_zmxmQDw7WTdtXa6MtHa7isMlJ1YOyIv_nwpmfRO4';
-            res.json({ username: req.query.username, token: token });
+        case 'persistdb':
+            // persistance service subs all, pubs none
+            jwt = generateToken(req.query.username, '1 year',
+                ["#"], null);
+            break;
+        case 'admin':
+            // admin is normal scene pub/sub, plus admin tasks
+            jwt = generateToken(req.query.username, '1 day',
+                [sadm, sobj], [sadm, sobj]);
+            break;
+        case 'editor':
+            // editor is normal scene pub/sub
+            jwt = generateToken(req.query.username, '1 day',
+                [sobj], [sobj]);
+            break;
+        case 'viewer':
+            // viewer is just sub
+            jwt = generateToken(req.query.username, '1 day',
+                [sobj], null);
             break;
         default:
-            res.json({ username: req.query.username, token: null });
+            jwt = null;
             break;
     }
+    res.json({ username: req.query.username, token: jwt });
 });
 
 server.listen(port, () => {
