d511cc1a527b5f33d32fb81d45985884170ea733
nampereira <3504501+nampereira@users.noreply.github.com>
2020-11-25 18:08:17 -0500

    New auth url

--- arena-defaults.js
+++ arena-defaults.js
@@ -15,6 +15,6 @@ const defaults = {
      mqttPath:["/mqtt/", "/mqtt1/", "/mqtt2/"],
      persistHost:"arena.andrew.cmu.edu",
      persistPath:"/persist/",
-     urlMqttAuth:"https://arena.andrew.cmu.edu:8888",
+     urlMqttAuth:"https://arena.andrew.cmu.edu/auth/",
      gAuthClientId:'173603117246-7lehsb3tpq4i17e7sla5bue1an4ps9t6.apps.googleusercontent.com'
 };
