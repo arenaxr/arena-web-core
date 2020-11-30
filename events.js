64114e484390073ffaa8812dffff7eb050703483
mwfarb <mwfarb@andrew.cmu.edu>
2020-11-30 11:53:32 -0500

    regex fix

--- events.js
+++ events.js
@@ -8,7 +8,7 @@
 const getSceneName = () => {
     let path = window.location.pathname.substring(1);
     if (defaults.supportDevFolders) {
-        path = path.replace(path.match(/\/(?:x|dev)\/([^\/]+)\/?/g)[0], "");
+        path = path.replace(path.match(/(?:x|dev)\/([^\/]+)\/?/g)[0], "");
     }
     if (path === '' || path === 'index.html') {
         return getUrlParam('scene', defaults.scenenameParam);
