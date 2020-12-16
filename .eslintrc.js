77ac6dae4e44985de5723f670e313d7f5c3af32c
Ivan Liang <r33t@cmu.edu>
2020-12-16 17:33:45 -0500

    Add modifications to style:
    - Line len 100
    - Tab/indent - 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,21 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
-  },
-  'extends': [
-    'google',
-  ],
-  'parserOptions': {
-    'ecmaVersion': 12,
-  },
-  'rules': {
-  },
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true,
+    },
+    'extends': [
+        'google',
+    ],
+    'parserOptions': {
+        'ecmaVersion': 12,
+    },
+    'rules': {
+        'indent': ['error', 4],
+        'max-len': ['error', {
+            code: 100,
+            tabWidth: 4,
+            ignoreUrls: true,
+        }],
+    },
 };
