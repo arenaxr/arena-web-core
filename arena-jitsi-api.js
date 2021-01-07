e2e21e72ff41e272207d98b29a8425453b63e2ae
mwfarb <mwfarb@andrew.cmu.edu>
2021-01-07 15:36:02 -0500

    more complete jitsi-safe room names

--- arena-jitsi-api.js
+++ arena-jitsi-api.js
@@ -5,8 +5,8 @@ const ARENAJitsiAPI = async function(jitsiServer) {
     // PRIVATE VARIABLES
     // ==================================================
 
-    // we use the scene name as the jitsi room name, handle namespace with '/' = '_'
-    const arenaConferenceName = globals.scenenameParam.toLowerCase().replace('/', '_');
+    // we use the scene name as the jitsi room name, handle RFC 3986 reserved chars as = '_'
+    const arenaConferenceName = globals.scenenameParam.toLowerCase().replace(/[!#$&'()*+,\/:;=?@[\]]/g, '_');
 
     const connectOptions = {
         hosts: {
