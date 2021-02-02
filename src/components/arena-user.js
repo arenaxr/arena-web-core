41746766fcf3fae7253682ac34cd055cf3a9986c
nampereira <3504501+nampereira@users.noreply.github.com>
2021-02-02 13:22:28 -0500

    Small fix

--- src/components/arena-user.js
+++ src/components/arena-user.js
@@ -181,7 +181,7 @@ AFRAME.registerComponent('arena-user', {
         videoCube.setAttribute('material', 'shader', 'flat');
         videoCube.setAttribute('src', `#${this.videoID}`); // video only! (no audio)
         videoCube.setAttribute('material-extras', 'encoding', 'sRGBEncoding');
-        screenShareEl.setAttribute('material-extras', 'needsUpdate', 'true');
+        videoCube.setAttribute('material-extras', 'needsUpdate', 'true');
 
         const videoCubeDark = document.createElement('a-box');
         videoCubeDark.setAttribute('id', this.videoID + 'cubeDark');
