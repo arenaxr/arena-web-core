612c0845a00aa2ae6b8100e7b30fa09593e1897f
Nuno Pereira <nampereira@gmail.com>
2021-02-08 19:34:35 +0000

    Retry applying color space settings

--- src/components/material-extras.js
+++ src/components/material-extras.js
@@ -2,7 +2,9 @@
 
 /**
  * Material extras component.
- * Allows to set extra material properties, namely texture encodind
+ * Allows to set extra material properties, namely texture encoding
+ * Timeout scheme in lack of better understanding of what causes/events to listen to
+ * be ensure properties are available
  */
 AFRAME.registerComponent('material-extras', {
     dependencies: ['material'],
@@ -12,32 +14,36 @@ AFRAME.registerComponent('material-extras', {
             'RGBM7Encoding', 'RGBM16Encoding', 'RGBDEncoding', 'BasicDepthPacking', 'RGBADepthPacking']},
         needsUpdate: {default: false},
     },
+    retryTimeouts: [1000, 2000, 5000, 10000],
     init: function() {
-        this.retry = true;
         this.update();
     },
     update: function() {
+        this.retryIndex = 0;
+        this.doUpdate();
+    },
+    doUpdate: function() {
         const mesh = this.el.getObject3D('mesh');
-        if (!mesh) console.error("could not find mesh!");
+
+        if (!mesh) {
+            console.error("could not find mesh!");
+            this.retryUpdate();
+        }
+
         if (mesh.material) {
             if (mesh.material.map) {
                 mesh.material.map.encoding = THREE[this.data.encoding];
                 mesh.material.needsUpdate = this.data.needsUpdate;
-            } else {
-                if (this.retry) {
-                    setTimeout(async () => {
-                        this.retry=false;
-                        this.update();
-                    }, 1000); // try again in a bit in case material is not ready yet
-                }
-            }
-        } else {
-            if (this.retry) {
-                setTimeout(async () => {
-                    this.retry=false;
-                    this.update();
-                }, 1000); // try again in a bit in case material is not ready yet
-            }
+            } else this.retryUpdate();
+        } else this.retryUpdate();
+    },
+    retryUpdate() {
+        if (this.retryIndex < this.retryTimeouts.length) {
+            setTimeout(async () => {
+                console.log("retry!");
+                this.retryIndex++;
+                this.doUpdate();
+            }, this.retryTimeouts[this.retryIndex]); // try again in a bit
         }
     },
 });
--- src/message-actions/create-update.js
+++ src/message-actions/create-update.js
@@ -185,6 +185,9 @@ export class CreateUpdate {
                     entityEl.setAttribute('material', 'src', data.url); // image src from url
                     if (!data.hasOwnProperty('material-extras')) {
                         // default images to sRGBEncoding, if not specified
+                        entityEl.setAttribute('material-extras', 'encoding', 'sRGBEncoding');   
+                        entityEl.setAttribute('material-extras', 'needsUpdate', 'true');
+                        /*
                         entityEl.setAttribute('material-extras', 'encoding', 'sRGBEncoding');
                         setTimeout(async () => {
                             entityEl.setAttribute('material-extras', 'needsUpdate', 'false');
@@ -192,6 +195,7 @@ export class CreateUpdate {
                         setTimeout(async () => {
                             entityEl.setAttribute('material-extras', 'needsUpdate', 'true');
                         }, 1000); // try again in a bit in case material is not ready yet    
+                        */
                     }
                 }
                 delete data.url;
