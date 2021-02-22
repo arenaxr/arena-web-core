601115cc8730197a23d1ed4c0201e6165177319e
Edward Lu <elu2@andrew.cmu.edu>
2021-02-22 00:45:08 +0000

    improve arrow look controls

--- src/aframe-mods/arrow-controls.js
+++ src/aframe-mods/arrow-controls.js
@@ -1,12 +1,44 @@
 /* global AFRAME */
 
-const MAX_DELTA_LOOK = 0.015;
-// const PI_2 = Math.PI / 2;
-keysPressed = {};
+const MAX_DELTA = 0.2;
 
 AFRAME.registerComponent('look-controls-arrow', {
+    schema: {
+        acceleration: {default: 1.1},
+    },
+
     init: function() {
+        this.keys = {};
+
+        this.onBlur = this.onBlur.bind(this);
+        this.onFocus = this.onFocus.bind(this);
+        this.onKeyDown = this.onKeyDown.bind(this);
+        this.onKeyUp = this.onKeyUp.bind(this);
+        this.onVisibilityChange = this.onVisibilityChange.bind(this);
+
+        this.attachVisibilityEventListeners();
+    },
+
+    play: function() {
         this.attachKeyEventListeners();
+        this.removeVisibilityEventListeners();
+    },
+
+    pause: function() {
+        this.keys = {};
+        this.removeKeyEventListeners();
+    },
+
+    attachVisibilityEventListeners: function() {
+        window.addEventListener('blur', this.onBlur);
+        window.addEventListener('focus', this.onFocus);
+        document.addEventListener('visibilitychange', this.onVisibilityChange);
+    },
+
+    removeVisibilityEventListeners: function() {
+        window.removeEventListener('blur', this.onBlur);
+        window.removeEventListener('focus', this.onFocus);
+        document.removeEventListener('visibilitychange', this.onVisibilityChange);
     },
 
     attachKeyEventListeners: function() {
@@ -19,32 +51,66 @@ AFRAME.registerComponent('look-controls-arrow', {
         window.removeEventListener('keyup', this.onKeyUp);
     },
 
+    onBlur: function() {
+        this.pause();
+    },
+
+    onFocus: function() {
+        this.play();
+    },
+
+    onVisibilityChange: function() {
+        if (document.hidden) {
+            this.onBlur();
+        } else {
+            this.onFocus();
+        }
+    },
+
     onKeyDown: function(event) {
-        keysPressed[event.key] = true;
+        this.keys[event.key] = true;
     },
 
     onKeyUp: function(event) {
-        delete keysPressed[event.key];
+        delete this.keys[event.key];
     },
 
     tick: function(time, delta) {
-        const myCamera = document.getElementById('my-camera');
-        const lookControls = myCamera.components['look-controls'];
+        const data = this.data;
+        const el = this.el;
+        const keys = this.keys;
+        const acceleration = data.acceleration;
 
-        const keys = keysPressed;
-        if (keys['ArrowLeft']) {
-            lookControls.yawObject.rotation.y += MAX_DELTA_LOOK;
-        }
-        if (keys['ArrowRight']) {
-            lookControls.yawObject.rotation.y -= MAX_DELTA_LOOK;
+        if (isEmptyObject(keys)) {
+            return;
         }
 
-        // if (keys['ArrowUp']) {
-        //    lookControls.pitchObject.rotation.x += MAX_DELTA_LOOK;
-        // }
-        // if (keys['ArrowDown']) {
-        // lookControls.pitchObject.rotation.x -= MAX_DELTA_LOOK;
-        // }
-        // lookControls.pitchObject.rotation.x = Math.max(-PI_2, Math.min(PI_2, lookControls.pitchObject.rotation.x));
+        // only move when focus is on body.
+        // allows user to type in chat, settings, etc without triggering movement.
+        // bit of a hack, not sure how aframe does it.
+        if (document.activeElement !== document.body) {
+            return;
+        }
+
+        delta = delta / 1000;
+
+        // If FPS too low, ignore.
+        if (delta > MAX_DELTA) {
+            return;
+        }
+
+        const lookControls = el.components['look-controls'];
+        if (keys.ArrowLeft) {
+            lookControls.yawObject.rotation.y += acceleration * delta;
+        }
+        if (keys.ArrowRight) {
+            lookControls.yawObject.rotation.y -= acceleration * delta;
+        }
     },
 });
+
+function isEmptyObject(keys) {
+    var key;
+    for (key in keys) { return false; }
+    return true;
+}
