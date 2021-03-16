f3a55f2c17f2637437b810e791091b790bb7daef
mwfarb <mwfarb@andrew.cmu.edu>
2021-03-15 21:04:20 -0400

    consolidate

--- src/components/gesture-detector.js
+++ src/components/gesture-detector.js
@@ -1,6 +1,8 @@
 /* global AFRAME, ARENA */
 
-import {ARENAUtils} from '../utils.js';
+import {
+    ARENAUtils
+} from '../utils.js';
 
 /**
  * Component that detects multi-finger touch gestures. Based off of work from 8th Wall at:
@@ -12,9 +14,7 @@ AFRAME.registerComponent('gesture-detector', {
         this.internalState = {
             previousState: null,
         };
-
         this.emitGestureEvent = this.emitGestureEvent.bind(this);
-
         window.addEventListener('touchstart', this.emitGestureEvent);
         window.addEventListener('touchend', this.emitGestureEvent);
         window.addEventListener('touchmove', this.emitGestureEvent);
@@ -29,22 +29,18 @@ AFRAME.registerComponent('gesture-detector', {
     emitGestureEvent(event) {
         const currentState = this.getTouchState(event);
 
-        // only send MQTT clientEvent for 2+ finger touches to avoid 1-finger common touch press-and-move
-        if (currentState.touchCount < 2) {
-            return;
-        }
+        // // only send MQTT clientEvent for 2+ finger touches to avoid 1-finger common touch press-and-move
+        // if (currentState.touchCount < 2) {
+        //     return;
+        // }
 
         const previousState = this.internalState.previousState;
-        const gestureContinues =
-            previousState &&
-            currentState &&
-            currentState.touchCount == previousState.touchCount;
+        const gestureContinues = previousState && currentState && currentState.touchCount == previousState.touchCount;
         const gestureEnded = previousState && !gestureContinues;
         const gestureStarted = currentState && !gestureContinues;
 
         if (gestureEnded) {
-            const eventName =
-                this.getEventPrefix(previousState.touchCount) + 'fingerend';
+            const eventName = this.getEventPrefix(previousState.touchCount) + 'fingerend';
             this.sendGesture(eventName, previousState);
             this.internalState.previousState = null;
         }
@@ -53,8 +49,7 @@ AFRAME.registerComponent('gesture-detector', {
             currentState.startTime = performance.now();
             currentState.startPosition = currentState.position;
             currentState.startSpread = currentState.spread;
-            const eventName =
-                this.getEventPrefix(currentState.touchCount) + 'fingerstart';
+            const eventName = this.getEventPrefix(currentState.touchCount) + 'fingerstart';
             this.sendGesture(eventName, currentState);
             this.internalState.previousState = currentState;
         }
@@ -69,10 +64,8 @@ AFRAME.registerComponent('gesture-detector', {
             if (currentState.spread) {
                 eventDetail.spreadChange = currentState.spread - previousState.spread;
             }
-
             // Update state with new data
             Object.assign(previousState, currentState);
-
             // Add state data to event detail
             Object.assign(eventDetail, previousState);
 
@@ -85,27 +78,17 @@ AFRAME.registerComponent('gesture-detector', {
         if (event.touches.length === 0) {
             return null;
         }
-
         // Convert event.touches to an array so we can use reduce
         const touchList = [];
-
         for (let i = 0; i < event.touches.length; i++) {
             touchList.push(event.touches[i]);
         }
-
         const touchState = {
             touchCount: touchList.length,
         };
-
         // Calculate center of all current touches
-        const centerPositionRawX =
-            touchList.reduce((sum, touch) => sum + touch.clientX, 0) /
-            touchList.length;
-
-        const centerPositionRawY =
-            touchList.reduce((sum, touch) => sum + touch.clientY, 0) /
-            touchList.length;
-
+        const centerPositionRawX = touchList.reduce((sum, touch) => sum + touch.clientX, 0) / touchList.length;
+        const centerPositionRawY = touchList.reduce((sum, touch) => sum + touch.clientY, 0) / touchList.length;
         touchState.positionRaw = {
             x: centerPositionRawX,
             y: centerPositionRawY,
@@ -121,26 +104,23 @@ AFRAME.registerComponent('gesture-detector', {
 
         // Calculate average spread of touches from the center point
         if (touchList.length >= 2) {
-            const spread =
-                touchList.reduce((sum, touch) => {
-                    return (
-                        sum +
-                        Math.sqrt(
-                            Math.pow(centerPositionRawX - touch.clientX, 2) +
-                            Math.pow(centerPositionRawY - touch.clientY, 2),
-                        )
-                    );
-                }, 0) / touchList.length;
+            const spread = touchList.reduce((sum, touch) => {
+                return (
+                    sum +
+                    Math.sqrt(
+                        Math.pow(centerPositionRawX - touch.clientX, 2) +
+                        Math.pow(centerPositionRawY - touch.clientY, 2),
+                    )
+                );
+            }, 0) / touchList.length;
 
             touchState.spread = spread * screenScale;
         }
-
         return touchState;
     },
 
     getEventPrefix(touchCount) {
         const numberNames = ['one', 'two', 'three', 'many'];
-
         return numberNames[Math.min(touchCount, 4) - 1];
     },
 
@@ -157,11 +137,20 @@ AFRAME.registerComponent('gesture-detector', {
             type: eventName,
             data: {
                 clickPos: clickPos,
-                //position: eventDetail.position,
-                ...eventDetail,
+                position: eventDetail.positionRaw,
+                //...eventDetail,
                 source: ARENA.camName,
             },
         };
+        if (eventDetail.positionChange) {
+            thisMsg.data.positionChange = eventDetail.positionChange;
+        }
+        if (eventDetail.spreadChange) {
+            thisMsg.data.spreadChange = eventDetail.spreadChange;
+        }
+        if (eventDetail.spreadChange) {
+            thisMsg.data.spreadChange = eventDetail.spreadChange;
+        }
         // publishing events attached to user id objects allows sculpting security
         ARENA.Mqtt.publish(ARENA.outputTopic + ARENA.camName, thisMsg);
     },
