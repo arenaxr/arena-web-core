c111edae661b834d136d65ec223adf43cf948695
Ivan Liang <r33t@cmu.edu>
2021-04-28 18:19:57 -0400

    fix: Disable startFrame from  n5ro/aframe-extras#337
    This somehow breaks animations interrupting another

--- src/components/vendor/animation-mixer.js
+++ src/components/vendor/animation-mixer.js
@@ -8,6 +8,12 @@
 
 /* global AFRAME, THREE */
 
+const LoopMode = {
+    once: THREE.LoopOnce,
+    repeat: THREE.LoopRepeat,
+    pingpong: THREE.LoopPingPong
+};
+
 AFRAME.components['animation-mixer'].Component.prototype.load = function(model) {
     const el = this.el;
     this.model = model;
@@ -27,3 +33,47 @@ AFRAME.components['animation-mixer'].Component.prototype.load = function(model) 
     });
     if (this.data.clip) this.update({});
 };
+
+AFRAME.components['animation-mixer'].Component.prototype.playAction = function() {
+    if (!this.mixer) return;
+
+    const model = this.model,
+        data = this.data,
+        clips = model.animations || (model.geometry || {}).animations || [];
+
+    if (!clips.length) return;
+
+    const re = wildcardToRegExp(data.clip);
+
+    for (let clip, i = 0; (clip = clips[i]); i++) {
+        if (clip.name.match(re)) {
+            const action = this.mixer.clipAction(clip, model);
+
+            action.enabled = true;
+            action.clampWhenFinished = data.clampWhenFinished;
+            if (data.duration) action.setDuration(data.duration);
+            if (data.timeScale !== 1) action.setEffectiveTimeScale(data.timeScale);
+            action
+            .setLoop(LoopMode[data.loop], data.repetitions)
+            .fadeIn(data.crossFadeDuration)
+            .play();
+            this.activeActions.push(action);
+            // this.mixer.setTime(data.startFrame / 1000);
+        }
+    }
+}
+
+/**
+ * Creates a RegExp from the given string, converting asterisks to .* expressions,
+ * and escaping all other characters.
+ */
+function wildcardToRegExp(s) {
+    return new RegExp('^' + s.split(/\*+/).map(regExpEscape).join('.*') + '$');
+}
+
+/**
+ * RegExp-escapes all characters in the given string.
+ */
+function regExpEscape(s) {
+    return s.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+}
