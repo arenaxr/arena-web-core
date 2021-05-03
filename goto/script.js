8231828359e84da9d71e00203a51823821ece58e
Ivan Liang <r33t@cmu.edu>
2021-05-03 19:52:32 -0400

    feat: Add simple scene randomizer
    - Use:
      ```
      data = btoa(JSON.stringify(
        {n:'myNameSpace',s:'myScene',r: 3}
      ));
      ```
      https://host/goto/?scene=<data>?otherparams=value
      Yields scene URLS:
      - `/myNameSpace/myScene_1`
      - `/myNameSpace/myScene_2`
      - `/myNameSpace/myScene_3`

---
+++ goto/script.js
@@ -0,0 +1,22 @@
+(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const sceneConfig = urlParams.get('scene');
+    urlParams.delete('scene');
+    try {
+        const config = JSON.parse(atob(sceneConfig));
+        // {n:'namespace', s: 'sceneId',r: 4}
+        const sceneKey = `${config.n}/${config.s}`;
+        const sceneHist = JSON.parse(localStorage.getItem('sceneHistory')) || {};
+        let randomSceneNum = sceneHist[sceneKey]?.lastRandom;
+        if (!randomSceneNum) {
+            randomSceneNum = Math.floor(Math.random() * (config.r) + 1);
+            sceneHist[sceneKey] = {...sceneHist[sceneKey], lastRandom: randomSceneNum};
+            localStorage.setItem('sceneHistory', JSON.stringify(sceneHist));
+        }
+        const sceneUrl = `/${config.n}/${config.s}_${randomSceneNum}?${urlParams.toString()}`;
+        window.location.replace(sceneUrl);
+    } catch (e) {
+        console.log(e);
+        window.location.replace('/');
+    }
+})();
