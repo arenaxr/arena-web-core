baf62eedeb2191ef304a3436b6079468b9d4f67c
mwfarb <mwfarb@andrew.cmu.edu>
2021-05-06 09:47:20 -0400

    Exit fullscreen framerate fix, fixed #251, fixed #197.

--- src/icons/index.js
+++ src/icons/index.js
@@ -6,6 +6,25 @@ import './style.css';
 
 const ICON_BTN_CLASS = 'arena-icon-button';
 
+// fullscreen exit handlers
+if (document.addEventListener) {
+    document.addEventListener('fullscreenchange', fullScreenExitHandler, false);
+    document.addEventListener('mozfullscreenchange', fullScreenExitHandler, false);
+    document.addEventListener('MSFullscreenChange', fullScreenExitHandler, false);
+    document.addEventListener('webkitfullscreenchange', fullScreenExitHandler, false);
+}
+
+/**
+ * Handle exit from full screen scenarios
+ */
+function fullScreenExitHandler() {
+    if (document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement !== null) {
+        // manually disable a-frame stats
+        const sceneEl = document.querySelector('a-scene');
+        sceneEl.removeAttribute('stats');
+    }
+}
+
 /**
  * Creates a button that will be displayed as an icon on the left of the screen
  * @param {string} initialImage name of initial image to be displayed
