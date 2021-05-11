7b61e6117f1c98e27feecca2f9f22af406f3c2ee
mwfarb <mwfarb@andrew.cmu.edu>
2021-05-11 12:27:13 -0400

    Add `noreticle` url param (#288)
    * Add noreticle url param

--- src/webxr/web-xr-viewer.js
+++ src/webxr/web-xr-viewer.js
@@ -4,6 +4,7 @@
  */
 
 window.addEventListener('enter-vr', async function(e) {
+    const urlParams = new URLSearchParams(window.location.search);
     const sceneEl = document.querySelector('a-scene');
     if (sceneEl.is('ar-mode')) {
         window.lastMouseTarget = undefined;
@@ -20,8 +21,12 @@ window.addEventListener('enter-vr', async function(e) {
             cursor.setAttribute('fuse', false);
             cursor.setAttribute('scale', '0.1 0.1 0.1');
             cursor.setAttribute('position', '0 0 -0.1');
-            cursor.setAttribute('color', '#555');
             cursor.setAttribute('max-distance', '10000');
+            if (urlParams.get('noreticle')) {
+                cursor.setAttribute('material', 'transparent: "true"; opacity: 0');
+            } else {
+                cursor.setAttribute('color', '#555');
+            }
             cursorParent.appendChild(cursor);
 
             // handle tap events
