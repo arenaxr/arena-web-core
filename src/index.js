a0ed571b2839fcaec320a0d84fd3d0dda6df9ac6
Nuno Pereira <nampereira@gmail.com>
2021-06-30 21:28:55 +0000

    Start mqtt console log

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@
  * @date 2020
  */
 
+import {ARENAMqttConsole} from './arena-console.js';
+
 // load order: AFRAME, ARENA, components that depend on AFRAME and ARENA
 import 'aframe'; // AFRAME
 import './arena.js'; // ARENA
@@ -13,3 +15,7 @@ import './ui/'; // 2D UI
 import './aframe-mods/'; // AFRAME modifications
 import './components/'; // custom AFRAME components
 import './webxr/'; // spedial handler for webxr devices
+
+// replace console with our logging
+// will queue messages until MQTT connection is available (indicated by console.setOptions())
+ARENAMqttConsole.init();
