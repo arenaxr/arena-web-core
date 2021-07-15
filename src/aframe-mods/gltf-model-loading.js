6418f6cbd428556bca8c0dc0108d948ec66436c9
Nuno Pereira <nampereira@gmail.com>
2021-07-15 18:11:25 +0000

    Rename model progress component

--- src/aframe-mods/gltf-model-loading.js
+++ src/aframe-mods/gltf-model-loading.js
@@ -19,8 +19,8 @@ AFRAME.components['gltf-model'].Component.prototype.update = function() {
 
     this.remove();
 
-    // register with gltf-model-progress system to handle model loading events
-    document.querySelector("a-scene").systems["gltf-model-progress"].registerGltf(el);
+    // register with model-progress system to handle model loading events
+    document.querySelector("a-scene").systems["model-progress"].registerModel(el, src);
 
     this.loader.load(src, function gltfLoaded(gltfModel) {
         self.model = gltfModel.scene || gltfModel.scenes[0];
