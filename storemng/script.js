2171553a3540a74b55e72a031a3d9c94598e83a3
mwfarb <mwfarb@andrew.cmu.edu>
2021-08-10 16:27:07 -0400

    force headers

--- storemng/script.js
+++ storemng/script.js
@@ -3,4 +3,23 @@
 $(document).ready(function() {
     // add page header
     $('#header').load('../header.html');
+
+
+    $.ajax({
+        url: '/storesrv',
+        // url: 'http://127.0.0.1:8080',
+        // data: {
+        //     username: 'mwfarb',
+        // },
+        type: 'GET',
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('X-Filebrowser-Auth', 'mwfarb');
+        },
+        crossDomain: true,
+        success: function(response) {
+            console.log('Success!');
+            // console.log(response);
+            document.myIframe.document.body.innerHTML = response;
+        },
+    });
 });
