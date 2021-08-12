6b6275e8969946d1fae2f939f76631b21033de95
mwfarb <mwfarb@andrew.cmu.edu>
2021-08-12 11:25:00 -0400

    restore proxy route and save debug

--- storemng/script.js
+++ storemng/script.js
@@ -4,22 +4,18 @@ $(document).ready(function() {
     // add page header
     $('#header').load('../header.html');
 
-
-    $.ajax({
-        url: '/storesrv',
-        // url: 'http://127.0.0.1:8080',
-        // data: {
-        //     username: 'mwfarb',
-        // },
-        type: 'GET',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader('X-Filebrowser-Auth', 'mwfarb');
-        },
-        crossDomain: true,
-        success: function(response) {
-            console.log('Success!');
-            // console.log(response);
-            document.myIframe.document.body.innerHTML = response;
-        },
-    });
+    // TODO(mwfarb): remove debug serving store below...
+    // $.ajax({
+    //     url: '/storesrv',
+    //     // url: 'http://127.0.0.1:8080',
+    //     type: 'GET',
+    //     beforeSend: function(xhr) {
+    //         xhr.setRequestHeader('X-Filebrowser-Auth', 'testusername1');
+    //     },
+    //     crossDomain: true,
+    //     success: function(response) {
+    //         console.log('Success!');
+    //         document.storeIframe.document.body.innerHTML = response;
+    //     },
+    // });
 });
