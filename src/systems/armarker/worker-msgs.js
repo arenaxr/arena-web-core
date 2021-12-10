22e19ebf43a3110a883b9a052f50ee3ddf89b070
Nuno Pereira <nampereira@gmail.com>
2021-12-10 19:30:50 +0000

    More cleanup; Making linter happy

--- src/systems/armarker/worker-msgs.js
+++ src/systems/armarker/worker-msgs.js
@@ -1,14 +1,14 @@
 const CVWorkerMsgs = {
-  type: {
+    type: {
     /* sent from worker */
-    INIT_DONE: 0,         // worker is ready
-    FRAME_RESULTS: 1,     // worker finished processing frame
-    NEXT_FRAME_REQ: 2,    // worker requests a new frame
-    /* sent to worker */
-    PROCESS_GSFRAME: 3,   // process grayscale image
-    KNOWN_MARKER_ADD: 4,  // indicate known marker data (size)
-    KNOWN_MARKER_DEL: 5,  // remove known marker data (size)
-  }
-}
+        INIT_DONE: 0, // worker is ready
+        FRAME_RESULTS: 1, // worker finished processing frame
+        NEXT_FRAME_REQ: 2, // worker requests a new frame
+        /* sent to worker */
+        PROCESS_GSFRAME: 3, // process grayscale image
+        KNOWN_MARKER_ADD: 4, // indicate known marker data (size)
+        KNOWN_MARKER_DEL: 5, // remove known marker data (size)
+    },
+};
 
 export {CVWorkerMsgs};
