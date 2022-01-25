97c1c4dd091e725c93a86bab1f55d091456971e5
Ivan Liang <r33t@cmu.edu>
2022-01-24 20:23:22 -0500

    build: revert to npm aframe-environment-component

--- src/components/index.js
+++ src/components/index.js
@@ -41,7 +41,7 @@ import './video-control.js';
 // from npm, when available
 import 'aframe-thickline-component'; // our version of aframe-meshline-component
 import 'webxr-polyfill'; // fallback for non-webXR browsers
-// import 'aframe-environment-component'; // pretty environments (npm version has a bug; imported from file instead)
+import 'aframe-environment-component'; // pretty environments
 import 'aframe-extras'; // gltf animations, components for controls, model loaders, pathfinding
 import 'aframe-physics-system'; //  Physics system
 import 'aframe-blink-controls'; // Controller teleport
@@ -53,7 +53,6 @@ import './vendor/nav-system.js'; // Override nav system
 // import 'aframe-spe-particles-component';
 
 // direct file imports
-import './vendor/aframe-environment-component.min.js'; // pretty environments (buggy on npm)
 import './vendor/aframe-multisrc-component.js'; // add separate image/video textures: https://github.com/elbobo/aframe-multisrc-component
 import './vendor/transparent-occlude.js'; // borrowed from aframe-render-order.js: https://github.com/supermedium/superframe#readme
 import './vendor/morphTarget.js'; // target and control a gltf model's morphTargets: https://github.com/elbobo/aframe-gltf-morph-component
