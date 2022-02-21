56a5b367b0dc7414aa58d0c482401cb7f7f402e1
Ivan <r33t@cmu.edu>
2022-02-21 11:42:46 -0500

    fix: apriltag wasm import

--- src/systems/armarker/apriltag-detector/apriltag.js
+++ src/systems/armarker/apriltag-detector/apriltag.js
@@ -13,7 +13,7 @@
  * @authors Nuno Pereira
  */
 
-import('./apriltag_wasm.js');
+import AprilTagWasm from './apriltag_wasm.js';
 
 // CV Worker message types (**copy of worker-msgs.js**)
 const CVWorkerMsgs = {
