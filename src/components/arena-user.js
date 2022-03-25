76ae19974bbf20cb687815db84305f95d0934a4b
Nuno Pereira <nampereira@gmail.com>
2022-03-25 11:12:31 +0000

    refactor: arena us for simpler tav culling handling in tick()

--- src/components/arena-user.js
+++ src/components/arena-user.js
@@ -14,7 +14,7 @@
  * @param {Object} gainNode
  * @private
  */
-async function enableChromeAEC(gainNode) {
+ async function enableChromeAEC(gainNode) {
     /**
      *  workaround for: https://bugs.chromium.org/p/chromium/issues/detail?id=687574
      *  1. grab the GainNode from the scene's THREE.AudioListener
@@ -143,6 +143,14 @@ AFRAME.registerComponent('arena-user', {
 
         this.bbox = new THREE.Box3();
 
+        // init attributes used in tick()
+        const myCam = document.getElementById('my-camera');
+        this.myCamPos = myCam.object3D.position;
+        this.entityPos = el.object3D.position;
+        const arenaCameraComponent = myCam.components['arena-camera'];
+        this.cameraVideoCulling = arenaCameraComponent.data.videoCulling;
+        if (this.cameraVideoCulling) this.cameraFrustum = arenaCameraComponent.frustum;
+
         this.tick = AFRAME.utils.throttleTick(this.tick, 1000, this);
     },
 
@@ -223,11 +231,13 @@ AFRAME.registerComponent('arena-user', {
             videoCubeDark.setAttribute('opacity', '0.8');
 
             el.appendChild(videoCubeDark);
+            this.videoCubeDark = videoCubeDark;
         } else {
             videoCube.setAttribute('scale', '0.9 1.5 0.02');
         }
 
         el.appendChild(videoCube);
+        this.videoCube = videoCube;
 
         this.headModel.setAttribute('visible', false);
     },
@@ -237,14 +247,12 @@ AFRAME.registerComponent('arena-user', {
         const data = this.data;
 
         // remove video cubes
-        const vidCube = document.getElementById(this.videoID + 'cube');
-        if (el.contains(vidCube)) {
-            el.removeChild(vidCube);
+        if (el.contains(this.videoCube)) {
+            el.removeChild(this.videoCube);
         }
 
-        const vidCubeDark = document.getElementById(this.videoID + 'cubeDark');
-        if (el.contains(vidCubeDark)) {
-            el.removeChild(vidCubeDark);
+        if (el.contains(this.videoCubeDark)) {
+            el.removeChild(this.videoCubeDark);
         }
 
         this.headModel.setAttribute('visible', true);
@@ -263,8 +271,7 @@ AFRAME.registerComponent('arena-user', {
 
             const jistiVideo = document.getElementById(this.videoID);
             if (jistiVideo) {
-                const vidCube = document.getElementById(this.videoID + 'cube');
-                if (!vidCube) {
+                if (!this.videoCube) {
                     this.drawVideoCube();
                 }
             }
@@ -391,39 +398,29 @@ AFRAME.registerComponent('arena-user', {
     },
 
     tick: function() {
-        const data = this.data;
-        const el = this.el;
-
         // do periodic a/v updates
         if (ARENA.Jitsi && ARENA.Jitsi.ready && data.jitsiId) {
             this.updateVideo();
             this.updateAudio();
         }
 
-        const myCam = document.getElementById('my-camera');
-        const camPos = myCam.object3D.position;
-        const entityPos = el.object3D.position;
-        const distance = camPos.distanceTo(entityPos);
+        const distance = this.myCamPos.distanceTo(this.entityPos);
 
+        // frustum culling for WebRTC video streams;
         if (this.videoID) {
-            // frustum culling for WebRTC video streams;
-            const cam = myCam.components['arena-camera'];
-            if (cam.data.videoCulling) { // video culling enabled ?
-                const vidCube = document.getElementById(this.videoID + 'cube');
-                let inFieldOfView = true;
-                if (el.contains(vidCube)) {
-                    const cam = myCam.components['arena-camera'];
-                    // TODO: check if we have to transform these bbox coordinates
-                    this.bbox.setFromObject(vidCube.object3D);
-                    inFieldOfView = cam.frustum.intersectsBox(this.bbox);
+            let inFieldOfView = true;
+            if (this.cameraVideoCulling) { // video culling enabled ?
+                if (el.contains(this.videoCube)) {
+                    // note: compute the world-axis-aligned bounding box of the video cube
+                    this.bbox.setFromObject(this.videoCube.object3D);
+                    inFieldOfView = this.cameraFrustum.intersectsBox(this.bbox);
                 }
-
-                // check if A/V cut off distance has been reached
-                if (!inFieldOfView || distance > ARENA.maxAVDist) {
-                    this.muteVideo();
-                } else {
-                    this.unmuteVideo();
-                }
+            }
+            // check if A/V cut off distance has been reached
+            if (!inFieldOfView || distance > ARENA.maxAVDist) {
+                this.muteVideo();
+            } else {
+                this.unmuteVideo();
             }
         }
 
