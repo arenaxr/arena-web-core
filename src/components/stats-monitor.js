4be01104c38155dc6b4443226c6525fb1e745497
mwfarb <mwfarb@andrew.cmu.edu>
2022-04-04 14:55:37 -0400

    add memory stats

--- src/components/stats-monitor.js
+++ src/components/stats-monitor.js
@@ -23,6 +23,12 @@ AFRAME.registerComponent('stats-monitor', {
         raf: { // A-Frame stats, Milliseconds needed to render a frame. (latency)
             default: 0,
         },
+        totalJSHeapSize: { // The total allocated heap size, in bytes. (Chrome/Edge only)
+            default: 0,
+        },
+        usedJSHeapSize: { // The currently active segment of JS heap, in bytes. (Chrome/Edge only)
+            default: 0,
+        },
     },
 
     init: function() {
@@ -35,8 +41,7 @@ AFRAME.registerComponent('stats-monitor', {
         this.el.sceneEl.setAttribute('stats', '');
     },
 
-    update: function() {
-    },
+    update: function() {},
 
     tick: function(time, timeDelta) {
         if (!this.rafDiv) {
@@ -51,17 +56,28 @@ AFRAME.registerComponent('stats-monitor', {
         }
         this.fps = parseFloat(this.fpsDiv.innerHTML, 10);
 
+        if (window.performance && window.performance.memory) {
+            const memory = window.performance.memory;
+            this.usedJSHeapSize = memory.usedJSHeapSize;
+            this.jsHeapSizeLimit = memory.jsHeapSizeLimit;
+        }
+
         if (ARENA.confstats) {
             if (ARENA && ARENA.Jitsi && ARENA.chat && ARENA.chat.settings) {
-                ARENAUtils.debug(({
+                const perfStats = {
                     jitsiStats: {
-                        arenaId: ARENA.idTag, // ARENA local participant id
-                        jitsiId: ARENA.Jitsi.jitsiId, // Jitsi local participant id
+                        arenaId: ARENA.idTag,
+                        jitsiId: ARENA.Jitsi.jitsiId,
                         renderFps: this.fps,
                         requestAnimationFrame: this.raf,
-                        stats: ARENA.chat.settings.stats, // Jitsi LOCAL_STATS_UPDATED result
+                        stats: ARENA.chat.settings.stats,
                     },
-                }));
+                };
+                if (window.performance && window.performance.memory) {
+                    perfStats.jitsiStats.usedJSHeapSize = this.usedJSHeapSize;
+                    perfStats.jitsiStats.jsHeapSizeLimit = this.jsHeapSizeLimit;
+                }
+                ARENAUtils.debug(perfStats);
             }
         }
     },
