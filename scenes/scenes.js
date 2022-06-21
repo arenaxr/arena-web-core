a2a64d8d3aa466f4b533091e87ecde30c0fdd457
mwfarb <mwfarb@andrew.cmu.edu>
2022-06-21 10:59:06 -0400

    restore all logic

--- scenes/scenes.js
+++ scenes/scenes.js
@@ -1,8 +1,26 @@
-/**
- * scenes-user.js - Scene Landing Full User handler
- */
-
 'use strict';
+
+window.publicButtons = []; // 1 or 2 buttons depending on auth
+
+function showEl(el, flex = false) {
+    const showClass = flex ? 'd-flex' : 'd-block';
+    el.classList.add(showClass);
+    el.classList.remove('d-none');
+}
+
+function hideEls(els, flex = false) {
+    const showClass = flex ? 'd-flex' : 'd-block';
+    for (let el of els) {
+        el.classList.add('d-none');
+        el.classList.remove(showClass);
+    }
+}
+
+function changePage(page = 'sceneSelect') {
+    if (window.location.hash !== page) {
+        window.location.hash = page;
+    }
+}
 
 // document.addEventListener('DOMContentLoaded', function() { // document.ready() equiv
 window.addEventListener('onauth', async function(e) {
@@ -10,11 +28,34 @@ window.addEventListener('onauth', async function(e) {
     const mqttToken = e.detail.mqtt_token;
     const auth = getAuthStatus();
     window.username = auth.username;
-    // auth.authenticated
-    // authType.textContent = auth.type;
-    // authUsername.textContent = auth.username;
-    // authFullname.textContent = auth.fullname;
-    // authEmail.textContent = auth.email;
+
+    // Rudimentary routing based on location hash
+    window.addEventListener('hashchange', function() {
+        const validRoutes = [
+            '#sceneSelect',
+            '#cloneScene',
+        ];
+        const routePage = validRoutes.includes(window.location.hash) ? window.location.hash : '#sceneSelect';
+        const pageEl = document.querySelector(routePage);
+        hideEls(document.querySelectorAll(`.routePage:not(${routePage})`));
+        showEl(pageEl, true);
+        pageEl.dispatchEvent(new Event('routePageLoaded'));
+    }, false);
+
+    document.getElementById('closeCloneScene').addEventListener('click', () => {
+        changePage('#sceneSelect');
+    })
+
+    $('select').each(function() {
+        $(this).select2({
+            theme: 'bootstrap4',
+            width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+            placeholder: $(this).data('placeholder'),
+            allowClear: Boolean($(this).data('allow-clear')),
+            closeOnSelect: !$(this).attr('multiple'),
+        });
+    });
+    $('select').val(null).trigger('change');
 
     const usernameSelect = document.getElementById('username');
     $(usernameSelect).text(username);
@@ -28,6 +69,13 @@ window.addEventListener('onauth', async function(e) {
     const copyUserSceneUrlBtn = document.getElementById('copyUserSceneUrlBtn')
 
     const clonePublicSceneBtn = document.getElementById('clonePublicSceneBtn');
+    const publicSceneSelect = document.getElementById('publicSceneSelect');
+    const publicSceneUrl = document.getElementById('publicSceneUrl');
+
+    const enterPublicSceneBtn = document.getElementById('enterPublicSceneBtn');
+
+    const userNoteSpan = document.getElementById('userNoteSpan');
+    const tabMyScenes = document.getElementById('myscenes-tab');
 
     const toggleUserSceneButtons = (toggle) => {
         [enterUserSceneBtn, cloneUserSceneBtn, deleteUserSceneBtn, copyUserSceneUrlBtn].forEach((btn) => {
@@ -36,6 +84,7 @@ window.addEventListener('onauth', async function(e) {
     }
 
     $(userSceneSelect).on('select2:select', checkUserSceneSelect);
+    $(publicSceneSelect).on('select2:select', checkPublicSceneSelect);
 
     function checkUserSceneSelect(e) {
         if (e.target.value) {
@@ -50,6 +99,22 @@ window.addEventListener('onauth', async function(e) {
         }
     }
 
+    function checkPublicSceneSelect(e) {
+        if (e.target.value) {
+            window.publicSceneId = e.target.value;
+            publicSceneUrl.value = `${window.location.origin}/${e.target.value}`
+            togglePublicSceneButtons(true);
+            console.log("valid public", e.target.value)
+        } else {
+            window.publicSceneId = '';
+            publicSceneUrl.value = '';
+            togglePublicSceneButtons(false);
+            console.log("invalid public", e.target.value)
+        }
+    }
+    enterPublicSceneBtn.addEventListener('click', () =>
+        window.location = publicSceneUrl.value
+    )
     copyUserSceneUrlBtn.addEventListener('click', () => {
         userSceneUrl.select();
         userSceneUrl.setSelectionRange(0, 99999); /* For mobile devices */
@@ -80,7 +145,7 @@ window.addEventListener('onauth', async function(e) {
                     }
                 });
             }).catch((err) => {
-                Swal.fire('Scene Delete Failed!', `Something went wrong!`, 'warning');
+                Swal.fire('Scene Delete Failed!', err.response.data, 'warning');
                 console.log(err);
             });
         }
@@ -98,11 +163,17 @@ window.addEventListener('onauth', async function(e) {
         changePage('cloneScene');
     });
 
+    window.userSceneId = '';
+    window.publicSceneId = '';
+    window.cloneSceneId = '';
+
     if (auth.authenticated) {
         window.publicButtons.push(clonePublicSceneBtn); // add clone option for full user
+        //tabMyScenes.parentElement.style.display = 'inline-block';
+    } else {
+        tabMyScenes.parentElement.style.display = 'none'; // anon users may not edit scenes
+        userNoteSpan.textContent = 'To create or clone scenes, please login with an authenticated account.';
     }
-
-
 
     /*  *********************** */
 
@@ -139,7 +210,7 @@ window.addEventListener('onauth', async function(e) {
             document.getElementById('cloneSceneCreated').classList.remove('d-none');
             newSceneNameInput.setAttribute('readonly', 'readonly')
         }).catch((err) => {
-            Swal.fire('Scene Clone Failed!', `Something went wrong!`, 'warning');
+            Swal.fire('Scene Clone Failed!', err.response.data, 'warning');
             console.log(err);
         });
     })
@@ -165,19 +236,23 @@ window.addEventListener('onauth', async function(e) {
             userSceneSelect.options.add(new Option(ns.name, ns.name));
         });
     }).catch((err) => {
-        Swal.fire('My Scene Load Failed!', `Something went wrong!`, 'warning');
+        Swal.fire('My Scene Load Failed!', err.response.data, 'warning');
         console.log(err);
     });
 
-    // Request public scenes...
-    axios.get('/persist/public/all', {
-        withCredentials: true
-    }).then((res) => {
-        res.data.forEach(ns => {
-            userSceneSelect.options.add(new Option(ns.name, ns.name));
-        });
-    }).catch((err) => {
-        Swal.fire('Public Scene Load Failed!', `Something went wrong!`, 'warning');
-        console.log(err);
-    });
+    // // Request public scenes...
+    // axios.get('/persist/public/all', {
+    //     withCredentials: true
+    // }).then((res) => {
+    //     res.data.forEach(ns => {
+    //         userSceneSelect.options.add(new Option(ns.name, ns.name));
+    //     });
+    // }).catch((err) => {
+    //     Swal.fire('Public Scene Load Failed!', err.response.data, 'warning');
+    //     console.log(err);
+    // });
+
+    if (!auth.authenticated) {
+        window.dispatchEvent(new Event('hashchange')); // Manually trigger initial hash routing
+    }
 });
