61a8d68f482a30df03b0ada5baf842ca61ee898b
mwfarb <mwfarb@andrew.cmu.edu>
2022-07-12 16:05:43 -0400

    draft url options picker

--- scenes/scenes.js
+++ scenes/scenes.js
@@ -67,6 +67,7 @@ window.addEventListener('onauth', async function(e) {
     const cloneUserSceneBtn = document.getElementById('cloneUserSceneBtn');
     const deleteUserSceneBtn = document.getElementById('deleteUserSceneBtn');
     const copyUserSceneUrlBtn = document.getElementById('copyUserSceneUrlBtn')
+    const advancedLinksUserBtn = document.getElementById('advancedLinksUserBtn')
 
     const clonePublicSceneBtn = document.getElementById('clonePublicSceneBtn');
     const publicSceneSelect = document.getElementById('publicSceneSelect');
@@ -74,6 +75,7 @@ window.addEventListener('onauth', async function(e) {
 
     const enterPublicSceneBtn = document.getElementById('enterPublicSceneBtn');
 
+    const advancedLinksDiv = document.getElementById('uri-builder')
     const userNoteSpan = document.getElementById('userNoteSpan');
     const tabMyScenes = document.getElementById('myscenes-tab');
 
@@ -87,7 +89,7 @@ window.addEventListener('onauth', async function(e) {
     }
 
     const toggleUserSceneButtons = (toggle) => {
-        [enterUserSceneBtn, cloneUserSceneBtn, deleteUserSceneBtn, copyUserSceneUrlBtn].forEach((btn) => {
+        [enterUserSceneBtn, cloneUserSceneBtn, deleteUserSceneBtn, copyUserSceneUrlBtn, advancedLinksUserBtn].forEach((btn) => {
             toggle ? btn.classList.remove('disabled') : btn.classList.add('disabled')
         })
     }
@@ -119,12 +121,12 @@ window.addEventListener('onauth', async function(e) {
             window.publicSceneId = e.target.value;
             publicSceneUrl.value = `${window.location.origin}/${e.target.value}`
             togglePublicSceneButtons(true);
-            console.log("valid public", e.target.value)
+            console.log('valid public', e.target.value)
         } else {
             window.publicSceneId = '';
             publicSceneUrl.value = '';
             togglePublicSceneButtons(false);
-            console.log("invalid public", e.target.value)
+            console.log('invalid public', e.target.value)
         }
     }
     enterPublicSceneBtn.addEventListener('click', () =>
@@ -133,11 +135,51 @@ window.addEventListener('onauth', async function(e) {
     copyUserSceneUrlBtn.addEventListener('click', () => {
         userSceneUrl.select();
         userSceneUrl.setSelectionRange(0, 99999); /* For mobile devices */
-        document.execCommand("copy");
+        document.execCommand('copy');
     })
     enterUserSceneBtn.addEventListener('click', () =>
         window.location = userSceneUrl.value
     )
+    advancedLinksUserBtn.addEventListener('click', () => {
+        advancedLinksDiv.hidden = !advancedLinksDiv.hidden;
+    })
+    // set listeners for advanced links URI-builder
+    var checkboxes = document.querySelectorAll('input[type=checkbox][name=uri-builder]');
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener('change', () => {
+            let uriSettings = [];
+            checkboxes.forEach(function(checkbox) {
+                console.log(checkbox.checked, checkbox.id);
+                if (checkbox.checked) {
+                    uriSettings.push(checkbox.id);
+                }
+            });
+            const sceneUrl = new URL(`${window.location.origin}/${window.userSceneId}`);
+            uriSettings.forEach(function(setting) {
+                switch (setting) {
+                    case 'armodeCheck':
+                        sceneUrl.searchParams.append('armode', '1');
+                        break;
+                    case 'vrCheck':
+                        sceneUrl.searchParams.append('vr', '1');
+                        break;
+                    case 'startLastPosCheck':
+                        sceneUrl.searchParams.append('startLastPos', '1');
+                        break;
+                    case 'noreticleCheck':
+                        sceneUrl.searchParams.append('noreticle', '1');
+                        break;
+                    case 'nonameCheck':
+                        sceneUrl.searchParams.append('noname', '1');
+                        break;
+                    case 'anonymousCheck':
+                        sceneUrl.searchParams.append('auth', 'anonymous');
+                        break;
+                }
+            });
+            userSceneUrl.value = sceneUrl.href;
+        })
+    });
 
     deleteUserSceneBtn.addEventListener('click', () => {
         if (confirm(`Are you sure you want to delete ${deleteUserSceneBtn.value}?`)) {
@@ -188,7 +230,7 @@ window.addEventListener('onauth', async function(e) {
 
     function resetCloneScene() {
         sourceScene.value = window.cloneSceneId;
-        newSceneNameInput.value = "";
+        newSceneNameInput.value = '';
         document.getElementById('cloneSceneCreated').classList.add('d-none');
         document.getElementById('doCloneSceneContainer').classList.remove('d-none');
     }
@@ -202,7 +244,7 @@ window.addEventListener('onauth', async function(e) {
     })
 
     document.getElementById('doCloneSceneBtn').addEventListener('click', () => {
-        const [namespace, sceneId] = sourceScene.value.split("/")
+        const [namespace, sceneId] = sourceScene.value.split('/')
         axios.post(`/persist/${window.username}/${newSceneNameInput.value}`, {
             action: 'clone',
             namespace,
@@ -223,7 +265,7 @@ window.addEventListener('onauth', async function(e) {
     copyCloneSceneUrlBtn.addEventListener('click', () => {
         cloneSceneUrl.select();
         cloneSceneUrl.setSelectionRange(0, 99999); /* For mobile devices */
-        document.execCommand("copy");
+        document.execCommand('copy');
     })
 
     document.getElementById('enterCloneSceneBtn').addEventListener('click', () => {
