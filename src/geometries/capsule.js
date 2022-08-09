259afdaa090240a1410b0d56738f5b237bd6fd93
mwfarb <mwfarb@andrew.cmu.edu>
2022-08-09 14:56:36 -0400

    feat(build): Added capsule geometry

---
+++ src/geometries/capsule.js
@@ -0,0 +1,28 @@
+/* global AFRAME */
+
+AFRAME.registerGeometry('capsule', {
+    schema: {
+        height : {default: 2, min: 0},
+        radius : {default: 1, min: 0},
+        segmentsHeight : {default: 18, min: 2},
+        segmentsRadial : {default: 36, min: 2},
+        capsSegments : {default: 8, min: 2},
+        thetaStart : {default: 0, min: 0},
+        thetaLength : {default: 360, min: 0},
+},
+
+init: function(data) {
+    this.geometry = new THREE.CapsuleBufferGeometry(
+        data.radius,
+        data.radius,
+        data.height,
+        data.segmentsRadial,
+        data.segmentsHeight,
+        data.capsSegments,
+        data.capsSegments,
+        data.thetaStart * (Math.PI / 180),
+        data.thetaLength * (Math.PI / 180)
+    );
+},
+
+});
---
+++ src/geometries/index.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview Import A-Frame Geometries used
+ *
+ * Open source software under the terms in /LICENSE
+ * Copyright (c) 2020, The CONIX Research Center. All rights reserved.
+ * @date Jan, 2021
+ */
+
+/**
+ * ARENA A-Frame geometries
+ */
+
+import './three-js-capsule-geometry/three-js-capsule-geometry.js';
+import './capsule.js';
