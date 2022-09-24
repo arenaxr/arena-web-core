8be2eb1871958bbf22a4b4d01b885c2cdb99075a
Edward Lu <elu2@andrew.cmu.edu>
2022-09-24 02:47:35 +0000

    force vp9

--- src/components/hybrid/render-client.js
+++ src/components/hybrid/render-client.js
@@ -8,6 +8,8 @@ const peerConnectionConfig = {
     ],
 };
 
+const invalidCodecs = ['video/VP8', 'video/red', 'video/ulpfec', 'video/rtx'];
+
 const supportsSetCodecPreferences = window.RTCRtpTransceiver &&
     'setCodecPreferences' in window.RTCRtpTransceiver.prototype;
 
@@ -122,7 +124,6 @@ AFRAME.registerComponent('render-client', {
         if (supportsSetCodecPreferences) {
             const transceiver = this.peerConnection.addTransceiver('video', {direction: 'recvonly'});
             const codecs = RTCRtpSender.getCapabilities('video').codecs;
-            const invalidCodecs = ['video/red', 'video/ulpfec', 'video/rtx'];
             const validCodecs = codecs.filter((codec) => !invalidCodecs.includes(codec.mimeType));
             console.log('validCodecs', validCodecs);
             transceiver.setCodecPreferences(validCodecs);
