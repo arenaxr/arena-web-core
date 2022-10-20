84ead001f7701e60452de4e639df4b84f3c6edba
mwfarb <mwfarb@andrew.cmu.edu>
2022-10-20 12:19:01 -0400

    smaller window

--- src/components/stats-monitor.js
+++ src/components/stats-monitor.js
@@ -70,12 +70,11 @@ AFRAME.registerComponent('stats-monitor', {
                 this.hudStatsText = document.createElement('a-text');
                 this.hudStatsText.setAttribute('id', 'myStats');
                 this.hudStatsText.setAttribute('position', '0 0 -1');
-                this.hudStatsText.setAttribute('scale', '0.25 0.25 0.25');
                 this.hudStatsText.setAttribute('side', 'double');
                 this.hudStatsText.setAttribute('align', 'left');
                 this.hudStatsText.setAttribute('anchor', 'center');
                 this.hudStatsText.setAttribute('color', '#cccccc');
-                this.hudStatsText.setAttribute('width', 4);
+                this.hudStatsText.setAttribute('width', 1);
                 camRoot.appendChild(this.hudStatsText);
             }
         }
@@ -101,7 +100,7 @@ AFRAME.registerComponent('stats-monitor', {
 
         if (ARENA && ARENA.hudstats && this.hudStatsText) {
             const pctHeap = Math.trunc(this.usedJSHeapSize / this.jsHeapSizeLimit / 100).toFixed(1);
-            let str = `fps: ${this.fps}\nraf: ${this.raf}\nused heap: ${this.usedJSHeapSize} (${pctHeap}%)\nmax heap: ${this.jsHeapSizeLimit}`;
+            let str = `FPS: ${this.fps}\nRAF: ${this.raf}\nUsed Heap: ${this.usedJSHeapSize} (${pctHeap}%)\nMax Heap: ${this.jsHeapSizeLimit}`;
             if (ARENA && ARENA.Jitsi && ARENA.chat && ARENA.chat.settings) {
                 str += `\n${ARENA.chat.getConnectionText(ARENA.getDisplayName(), ARENA.chat.settings.stats)}`;
             }
