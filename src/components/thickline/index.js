9b705b7aa9f400972fa541ca9cd42126519dd902
Ivan Liang <r33t@cmu.edu>
2023-03-03 02:53:03 -0500

    Add preset lineWidthSyles
    lineWidthStyle must now be a valid key, or style will default to constant width

--- src/components/thickline/index.js
+++ src/components/thickline/index.js
@@ -7,11 +7,30 @@ THREE.MeshLine = ml.MeshLine;
 THREE.MeshLineMaterial = ml.MeshLineMaterial;
 
 
+const lineWidthStylers = {
+    'grow': function(p) {
+        return p;
+    },
+    'shrink': function(p) {
+        return 1 - p;
+    },
+    'center-sharp': function(p) {
+        return 1 - Math.abs(2 * p - 1);
+    },
+    'center-smooth': function(p) {
+        Math.sin( p * 3.1415 );
+    },
+    'sine-wave': function(p) {
+        return 0.5 + 0.5 * Math.sin( (p - 0.5) * 2 * 3.1415 * 10 );
+    },
+};
+
+
 AFRAME.registerComponent('thickline', {
     schema: {
         color: {default: '#000'},
         lineWidth: {default: 10},
-        lineWidthStyler: {default: ''},
+        lineWidthStyler: {default: ''}, // One of lineWidthStyles above
         sizeAttenuation: {default: 0},
         near: {default: 0.1},
         far: {default: 1000},
@@ -71,7 +90,7 @@ AFRAME.registerComponent('thickline', {
 
         geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
 
-        const widthFn = function() {
+        const widthFn = lineWidthStylers[this.data.lineWidthStyler] ?? function() {
             return 1;
         };
         // ? try {var w = widthFn(0);} catch(e) {warn(e);}
