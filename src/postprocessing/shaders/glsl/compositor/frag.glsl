f7f83666609f059084b12485fa7ed6c84712c71b
Edward Lu <lued2018@gmail.com>
2023-06-16 17:54:44 +0000

    put back srgb in hybrid

--- src/postprocessing/shaders/glsl/compositor/frag.glsl
+++ src/postprocessing/shaders/glsl/compositor/frag.glsl
@@ -206,10 +206,6 @@ void main() {
         remoteDepth = readDepthRemote( tRemoteFrame, coordRemoteDepth );
     }
 
-// #ifdef IS_SRGB
-//         localColor = LinearTosRGB(localColor);
-// #endif
-
     vec4 color = localColor;
     // if (!targetWidthGreater ||
     //     (targetWidthGreater && paddingLeft <= vUv.x && vUv.x <= paddingRight)) {
@@ -234,6 +230,10 @@ void main() {
     // color = vec4(remoteColor.rgb, 1.0);
     // color = vec4(localColor.rgb, 1.0);
     gl_FragColor = color;
+
+#ifdef IS_SRGB
+    gl_FragColor = LinearTosRGB(gl_FragColor);
+#endif
 
     // gl_FragColor.rgb = vec3(localDepth);
     // gl_FragColor.a = 1.0;
