22ccca40184466ff40a84ddaf3575b0fc6b5ac3a
Ivan Liang <r33t@cmu.edu>
2023-06-19 15:43:03 -0400

    Add FXAA effect

--- src/constants/effects.js
+++ src/constants/effects.js
@@ -1,9 +1,11 @@
 import UnrealBloomPass from '../postprocessing/passes/unreal-bloom-pass';
 import GlitchPass from '../postprocessing/passes/glitch-pass';
+import FXAAPass from '../postprocessing/passes/fxaa-pass';
 
 const EFFECTS = {
     bloom: UnrealBloomPass,
     glitch: GlitchPass,
+    fxaa: FXAAPass,
 };
 
 export default EFFECTS;
