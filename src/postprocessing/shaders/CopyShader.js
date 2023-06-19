adb81d94711e88d39d4958f5cbe38fea2c25f3cf
Ivan Liang <r33t@cmu.edu>
2023-06-19 15:52:26 -0400

    Condense shader whitespace

--- src/postprocessing/shaders/CopyShader.js
+++ src/postprocessing/shaders/CopyShader.js
@@ -9,31 +9,20 @@ const CopyShader = {
     },
 
     vertexShader: /* glsl */ `
-
-		varying vec2 vUv;
-
-		void main() {
-
-			vUv = uv;
-			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-
-		}`,
+    varying vec2 vUv;
+    void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    }`,
 
     fragmentShader: /* glsl */ `
-
-		uniform float opacity;
-
-		uniform sampler2D tDiffuse;
-
-		varying vec2 vUv;
-
-		void main() {
-
-			gl_FragColor = texture2D( tDiffuse, vUv );
-			gl_FragColor.a *= opacity;
-
-
-		}`,
+    uniform float opacity;
+    uniform sampler2D tDiffuse;
+    varying vec2 vUv;
+    void main() {
+        gl_FragColor = texture2D( tDiffuse, vUv );
+        gl_FragColor.a *= opacity;
+    }`,
 };
 
 export default CopyShader;
