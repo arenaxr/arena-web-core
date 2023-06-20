d1efc6c0ad92ad96d18f0a53b8fddcee00874d3e
Ivan Liang <r33t@cmu.edu>
2023-06-20 05:21:54 -0400

    Add SMAA effect

--- src/constants/effects.js
+++ src/constants/effects.js
@@ -3,6 +3,7 @@ import GlitchPass from '../postprocessing/passes/glitch-pass';
 import FXAAPass from '../postprocessing/passes/fxaa-pass';
 import SAOPass from '../postprocessing/passes/sao-pass';
 import SSAOPass from '../postprocessing/passes/ssao-pass';
+import SMAAPass from '../postprocessing/passes/sma-pass';
 
 const EFFECTS = {
     bloom: UnrealBloomPass,
@@ -10,6 +11,7 @@ const EFFECTS = {
     fxaa: FXAAPass,
     sao: SAOPass,
     ssao: SSAOPass,
+    smaa: SMAAPass,
 };
 
 export default EFFECTS;
