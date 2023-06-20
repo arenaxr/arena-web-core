de2dbed1a70e46e43bb43cb3c36dc04edc6ca183
Ivan Liang <r33t@cmu.edu>
2023-06-20 04:26:40 -0400

    Add SSAO pass

--- src/constants/effects.js
+++ src/constants/effects.js
@@ -2,12 +2,14 @@ import UnrealBloomPass from '../postprocessing/passes/unreal-bloom-pass';
 import GlitchPass from '../postprocessing/passes/glitch-pass';
 import FXAAPass from '../postprocessing/passes/fxaa-pass';
 import SAOPass from '../postprocessing/passes/sao-pass';
+import SSAOPass from '../postprocessing/passes/ssao-pass';
 
 const EFFECTS = {
     bloom: UnrealBloomPass,
     glitch: GlitchPass,
     fxaa: FXAAPass,
     sao: SAOPass,
+    ssao: SSAOPass,
 };
 
 export default EFFECTS;
