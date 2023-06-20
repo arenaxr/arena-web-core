3c0765b2e4e589de1dbdf0135e5f6ad2d5c19c40
Ivan Liang <r33t@cmu.edu>
2023-06-20 04:12:07 -0400

    Add SAO pass
    Move FXAA down to last in effects opts

--- src/constants/effects.js
+++ src/constants/effects.js
@@ -1,11 +1,13 @@
 import UnrealBloomPass from '../postprocessing/passes/unreal-bloom-pass';
 import GlitchPass from '../postprocessing/passes/glitch-pass';
 import FXAAPass from '../postprocessing/passes/fxaa-pass';
+import SAOPass from '../postprocessing/passes/sao-pass';
 
 const EFFECTS = {
     bloom: UnrealBloomPass,
     glitch: GlitchPass,
     fxaa: FXAAPass,
+    sao: SAOPass,
 };
 
 export default EFFECTS;
