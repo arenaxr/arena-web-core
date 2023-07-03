2ed5f2ba7f9fa1ff185e477ec11d5811efa23e53
Ivan Liang <r33t@cmu.edu>
2023-07-03 13:20:26 -0400

    Early return for build page on anon-kickout
    The thenable from Swal.Fire is not blocking

--- build/main.js
+++ build/main.js
@@ -826,6 +826,7 @@ window.addEventListener('onauth', async (e) => {
             allowEscapeKey: false,
             allowOutsideClick: false,
         }).then(ARENAAUTH.signOut);
+        return;
     }
 
     arenaHostLbl.value = hostData.host;
