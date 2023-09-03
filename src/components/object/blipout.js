c572154d38bff353d6f1fa410417d7d0d0e06567
Ivan Liang <r33t@cmu.edu>
2023-09-03 12:27:09 -0400

    Add disk, ring variants of meshplane (def rect)

--- src/components/object/blipout.js
+++ src/components/object/blipout.js
@@ -4,9 +4,11 @@ AFRAME.registerComponent('blipout', {
     schema: {
         enabled: { type: 'boolean', default: true },
         duration: { type: 'number', default: 750 },
+        geometry: { type: 'string', default: 'rect' }, // [rect, disk, ring]
     },
     blip() {
         const {
+            data,
             el,
             el: { object3D, sceneEl },
         } = this;
@@ -36,33 +38,51 @@ AFRAME.registerComponent('blipout', {
         const midY = (minY + maxY) / 2;
         const width = bbox.max.x - bbox.min.x;
         const depth = bbox.max.z - bbox.min.z;
+        const radius = Math.max(width, depth) / 2;
 
         // Create clipping planes
         const planeBot = new THREE.Plane(new THREE.Vector3(0, 1, 0), -minY); // Clips everything below
         const planeTop = new THREE.Plane(new THREE.Vector3(0, -1, 0), maxY); // Clips everything above
 
+        const planeMeshMaterial = new THREE.MeshPhongMaterial({
+            color: 0x049ef4,
+            emissive: 0x000080,
+            specular: 0xffffff,
+            transparent: true,
+            opacity: 0.75,
+            shininess: 100,
+            side: THREE.DoubleSide,
+        });
+
+        switch (data.geometry) {
+            case 'disk': {
+                // 2d disk
+                this.meshPlaneBot = new THREE.Mesh(new THREE.RingGeometry(0, radius), planeMeshMaterial);
+                break;
+            }
+            case 'ring': {
+                // 2d ring
+                this.meshPlaneBot = new THREE.Mesh(
+                    new THREE.RingGeometry(radius, Math.min(radius + 0.15, radius * 1.1)),
+                    planeMeshMaterial
+                );
+                break;
+            }
+            default: // 2d rect
+                this.meshPlaneBot = new THREE.Mesh(new THREE.PlaneGeometry(width, depth), planeMeshMaterial);
+        }
+
         // Add visible mesh planes to match clipping
-        const meshPlaneBot = new THREE.Mesh(
-            new THREE.PlaneGeometry(width, depth),
-            new THREE.MeshPhongMaterial({
-                color: 0x049ef4,
-                emissive: 0x000080,
-                specular: 0xffffff,
-                transparent: true,
-                opacity: 0.75,
-                shininess: 100,
-                side: THREE.DoubleSide,
-            })
-        );
-        bbox.getCenter(meshPlaneBot.position); // align in world pos
-        meshPlaneBot.rotation.x = -Math.PI / 2; // rotate flat
 
-        const meshPlaneTop = meshPlaneBot.clone(); // clone bottom
+        bbox.getCenter(this.meshPlaneBot.position); // align in world pos
+        this.meshPlaneBot.rotation.x = -Math.PI / 2; // rotate horizontal flat
 
-        meshPlaneBot.position.y = minY;
-        meshPlaneTop.position.y = maxY;
+        this.meshPlaneTop = this.meshPlaneBot.clone(); // clone bottom as top
 
-        sceneEl.object3D.add(meshPlaneBot, meshPlaneTop); // Add to sceneroot for world space
+        this.meshPlaneBot.position.y = minY; // Only variation is difference in y pos
+        this.meshPlaneTop.position.y = maxY;
+
+        sceneEl.object3D.add(this.meshPlaneBot, this.meshPlaneTop); // Add to sceneroot for world space
 
         matTargets.forEach((matTarget) => {
             /* eslint-disable no-param-reassign */
@@ -72,20 +92,20 @@ AFRAME.registerComponent('blipout', {
         });
 
         AFRAME.ANIME({
-            targets: [planeBot, meshPlaneBot.position], // constant, y ignored in vec3, plane respectively
+            targets: [planeBot, this.meshPlaneBot.position], // constant, y ignored in vec3, plane respectively
             constant: -midY,
             y: midY,
             easing: 'easeInOutSine',
-            duration: this.data.duration,
+            duration: data.duration,
         });
         AFRAME.ANIME({
-            targets: [planeTop, meshPlaneTop.position],
+            targets: [planeTop, this.meshPlaneTop.position],
             constant: midY,
             y: midY,
             easing: 'easeInOutSine',
-            duration: this.data.duration + 1, // ensure later than bottom
+            duration: data.duration + 1, // ensure later than bottom
             complete: () => {
-                sceneEl.object3D.remove(meshPlaneBot, meshPlaneTop);
+                sceneEl.object3D.remove(this.meshPlaneBot, this.meshPlaneTop);
                 el.remove.bind(el)();
             },
         });
