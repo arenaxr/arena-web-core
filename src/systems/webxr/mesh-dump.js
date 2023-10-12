a262ffaec02e59a64f91121cfe12fa11d61f8352
Ivan Liang <r33t@cmu.edu>
2023-10-12 16:52:07 -0400

    retry after empty detectedMeshes

--- src/systems/webxr/mesh-dump.js
+++ src/systems/webxr/mesh-dump.js
@@ -3,6 +3,7 @@
 AFRAME.registerSystem('mesh-dump', {
     init() {
         const { sceneEl } = this;
+
         if (ARENA.params.debugMesh) {
             sceneEl.renderer.xr.addEventListener('sessionstart', () => {
                 if (sceneEl.is('ar-mode')) {
@@ -11,17 +12,27 @@ AFRAME.registerSystem('mesh-dump', {
                 }
             });
         }
+
+        this.onRAF = this.onRAF.bind(this);
+        this.webXRSessionStarted = this.webXRSessionStarted.bind(this);
     },
     async webXRSessionStarted(xrSession) {
         if (xrSession === undefined) return;
-        xrSession.requestAnimationFrame((_time, frame) => {
-            if (frame.detectedMeshes) {
+        xrSession.requestAnimationFrame(this.onRAF);
+    },
+    async onRAF(_time, frame) {
+        if (frame.detectedMeshes === undefined) return;
+        if (frame.detectedMeshes.size === 0) {
+            // First may be empty
+            this.sceneEl.xrSession.requestAnimationFrame(this.onRAF);
+        } else {
+            console.log(frame.detectedMeshes);
+            frame.detectedMeshes.forEach((mesh) => {
                 ARENA.Mqtt.publish(
                     `${ARENA.defaults.realm}/proc/debug/${ARENA.namespacedScene}`,
-                    JSON.stringify(frame.detectedMeshes)
+                    JSON.stringify({ vertices: mesh.vertices, indices: mesh.indices, semanticLabel: mesh })
                 );
-                console.log(frame.detectedMeshes);
-            }
-        });
+            });
+        }
     },
 });
