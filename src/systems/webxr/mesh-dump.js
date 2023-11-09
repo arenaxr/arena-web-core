9cfcf8890671e1af949e4025d9c4d9094474be80
Ivan Liang <r33t@cmu.edu>
2023-11-09 17:39:35 -0500

    Try to relocate pos (not rot) from ref_floor

--- src/systems/webxr/mesh-dump.js
+++ src/systems/webxr/mesh-dump.js
@@ -1,4 +1,12 @@
-/* global AFRAME, ARENA */
+/* global AFRAME, ARENA, THREE */
+
+function computeCentroid(points, centroid) {
+    centroid.set(0, 0);
+    points.forEach((p) => {
+        centroid.add(p);
+    });
+    centroid.divideScalar(points.length);
+}
 
 AFRAME.registerSystem('mesh-dump', {
     init() {
@@ -21,34 +29,58 @@ AFRAME.registerSystem('mesh-dump', {
         xrSession.requestAnimationFrame(this.onRAF);
     },
     async onRAF(_time, frame) {
-        if (frame.detectedMeshes === undefined) return;
-        if (frame.detectedMeshes.size === 0) {
+        if (frame.detectedPlanes === undefined) return;
+        if (frame.detectedPlanes.size === 0) {
             // First may be empty
             this.sceneEl.xrSession.requestAnimationFrame(this.onRAF);
         } else {
-            const xrRefSpace = this.sceneEl.renderer.xr.getReferenceSpace();
-            frame.detectedMeshes.forEach((mesh) => {
-                ARENA.Mqtt.publish(
-                    `${ARENA.defaults.realm}/proc/debug/${ARENA.namespacedScene}`,
-                    JSON.stringify({
-                        vertices: mesh.vertices,
-                        indices: mesh.indices,
-                        semanticLabel: mesh.semanticLabel,
-                        meshPose: frame.getPose(mesh.meshSpace, xrRefSpace).transform.matrix,
-                    })
-                );
-            });
-            frame.detectedPlanes.forEach((plane) => {
-                ARENA.Mqtt.publish(
-                    `${ARENA.defaults.realm}/proc/debug/${ARENA.namespacedScene}`,
-                    JSON.stringify({
-                        polygon: plane.polygon,
-                        orientation: plane.orientation,
-                        semanticLabel: plane.semanticLabel,
-                        planePose: frame.getPose(plane.planeSpace, xrRefSpace).transform.matrix,
-                    })
-                );
-            });
+            const refFloor = document.getElementById('ref_floor');
+            if (refFloor) {
+                let floorPlane;
+                // eslint-disable-next-line no-restricted-syntax
+                for (const plane of frame.detectedPlanes) {
+                    if (plane.semanticLabel === 'floor') {
+                        floorPlane = plane;
+                        break;
+                    }
+                }
+                const xrRefSpace = this.sceneEl.renderer.xr.getReferenceSpace();
+                const planePose = frame.getPose(floorPlane.planeSpace, xrRefSpace).transform.matrix;
+                const planePos = new THREE.Vector3();
+                planePos.setFromMatrix4(planePose);
+                // const dVectors = floorPlane.polygon.map((p) => new THREE.Vector2(p.x, p.y));
+                // dVectors.pop(); // Remove loop-closing end-point
+                // const floorCentroid = new THREE.Vector2();
+                // computeCentroid(dVectors, floorCentroid);
+                const offset = new THREE.Vector3(refFloor.x, refFloor.y, refFloor.z);
+                // offset.sub(new THREE.Vector3(floorCentroid.x, 0, floorCentroid.y));
+                offset.sub(planePos);
+                offset.y = 0; // Don't move vertically
+                ARENA.utils.relocateUserCamera(offset);
+            } else {
+                frame.detectedMeshes.forEach((mesh) => {
+                    ARENA.Mqtt.publish(
+                        `${ARENA.defaults.realm}/proc/debug/${ARENA.namespacedScene}`,
+                        JSON.stringify({
+                            vertices: mesh.vertices,
+                            indices: mesh.indices,
+                            semanticLabel: mesh.semanticLabel,
+                            meshPose: frame.getPose(mesh.meshSpace, xrRefSpace).transform.matrix,
+                        })
+                    );
+                });
+                frame.detectedPlanes.forEach((plane) => {
+                    ARENA.Mqtt.publish(
+                        `${ARENA.defaults.realm}/proc/debug/${ARENA.namespacedScene}`,
+                        JSON.stringify({
+                            polygon: plane.polygon,
+                            orientation: plane.orientation,
+                            semanticLabel: plane.semanticLabel,
+                            planePose: frame.getPose(plane.planeSpace, xrRefSpace).transform.matrix,
+                        })
+                    );
+                });
+            }
         }
     },
 });
