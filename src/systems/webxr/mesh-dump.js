830901f709e62da4ff9416dfd74c1b5e5a6d2aa7
Ivan Liang <r33t@cmu.edu>
2023-12-05 17:12:03 -0500

    Refactor mesh-dump with schema

--- src/systems/webxr/mesh-dump.js
+++ src/systems/webxr/mesh-dump.js
@@ -1,41 +1,17 @@
-/* global AFRAME, ARENA, THREE */
+/* global AFRAME, ARENA */
 
 import { Packr } from 'msgpackr';
-import { ARENA_EVENTS } from '../../constants';
-
-AFRAME.registerSystem('debug-ui', {
-    init() {
-        ARENA.debugXR = console.log;
-        if (ARENA.params.debugUI) {
-            ARENA.events.addMultiEventListener([ARENA_EVENTS.ARENA_LOADED], () => {
-                const debugCard = document.createElement('a-entity');
-                debugCard.setAttribute('arenaui-card', {
-                    title: 'Debug',
-                    body: '',
-                    fontSize: 0.018,
-                    widthScale: '0.5',
-                });
-                debugCard.setAttribute('position', { x: 0, y: 0.2, z: -1 });
-                document.getElementById('my-camera').appendChild(debugCard);
-                ARENA.debugXR = (text, newline = true) => {
-                    const prevText = debugCard.getAttribute('arenaui-card').body;
-                    if (text === undefined) {
-                        debugCard.setAttribute('arenaui-card', { body: '' });
-                    } else {
-                        const n = newline ? '\n' : '';
-                        debugCard.setAttribute('arenaui-card', { body: `${prevText}${n}${text}` });
-                    }
-                };
-            });
-        }
-    },
-});
 
 AFRAME.registerSystem('mesh-dump', {
     schema: {
-        directionObjectName: { type: 'string', default: 'door' },
+        publishMeshes: { type: 'boolean', default: true },
+        onlyGlobalMesh: { type: 'boolean', default: true },
+        publishPlanes: { type: 'boolean', default: false },
+        publishTopicBase: {
+            type: 'string',
+            default: `${ARENA.defaults.realm}/d/${ARENA.namespacedScene}/${ARENA.camName}`,
+        },
     },
-
     init() {
         const { sceneEl } = this;
 
@@ -60,42 +36,47 @@ AFRAME.registerSystem('mesh-dump', {
         xrSession.requestAnimationFrame(this.onRAF);
     },
     async onRAF(_time, frame) {
-        if (frame.detectedPlanes === undefined || frame.detectedMeshes === undefined) return;
-        if (frame.detectedPlanes.size === 0 || frame.detectedMeshes.size === 0) {
+        const {
+            data: { publishMeshes, publishPlanes, publishTopicBase, onlyGlobalMesh },
+        } = this;
+        if (
+            (publishPlanes && frame.detectedPlanes === undefined) ||
+            (publishMeshes && frame.detectedMeshes === undefined)
+        )
+            return;
+        if ((publishPlanes && frame.detectedPlanes.size === 0) || (publishMeshes && frame.detectedMeshes.size === 0)) {
             // First may be empty
             this.sceneEl.xrSession.requestAnimationFrame(this.onRAF);
         } else {
-            ARENA.debugXR('Found plane and mesh', false);
+            ARENA.debugXR('Found plane and/or mesh', false);
             const xrRefSpace = this.sceneEl.renderer.xr.getReferenceSpace();
-            frame.detectedMeshes.forEach((mesh) => {
-                if (mesh.semanticLabel === 'global mesh') {
+            if (publishMeshes) {
+                frame.detectedMeshes.forEach((mesh) => {
+                    if (onlyGlobalMesh && mesh.semanticLabel !== 'global mesh') {
+                        return;
+                    }
                     const msg = this.packr.pack({
                         vertices: Object.values(mesh.vertices),
                         indices: Object.values(mesh.indices),
                         semanticLabel: mesh.semanticLabel,
                         meshPose: Object.values(frame.getPose(mesh.meshSpace, xrRefSpace).transform.matrix),
                     });
-                    ARENA.debugXR(' packing and publishing global mesh');
-                    ARENA.Mqtt.publish(
-                        `${ARENA.defaults.realm}/proc/debug/${ARENA.namespacedScene}/${ARENA.camName}/meshes`,
-                        msg,
-                        undefined,
-                        undefined,
-                        true
-                    );
-                }
-            });
-            // frame.detectedPlanes.forEach((plane) => {
-            //     ARENA.Mqtt.publish(
-            //         `${ARENA.defaults.realm}/proc/debug/${ARENA.namespacedScene}/${ARENA.camName}/planes`,
-            //         JSON.stringify({
-            //             polygon: plane.polygon,
-            //             orientation: plane.orientation,
-            //             semanticLabel: plane.semanticLabel,
-            //             planePose: frame.getPose(plane.planeSpace, xrRefSpace).transform.matrix,
-            //         })
-            //     );
-            // });
+                    ARENA.debugXR(`Packing and publishing ${mesh.semanticLabel}`);
+                    ARENA.Mqtt.publish(`${publishTopicBase}/meshes`, msg, undefined, undefined, true);
+                });
+            }
+            if (publishPlanes) {
+                frame.detectedPlanes.forEach((plane) => {
+                    const msg = this.packr.pack({
+                        polygon: plane.polygon,
+                        orientation: plane.orientation,
+                        semanticLabel: plane.semanticLabel,
+                        planePose: frame.getPose(plane.planeSpace, xrRefSpace).transform.matrix,
+                    });
+                    ARENA.debugXR(`Packing and publishing ${plane.semanticLabel}`);
+                    ARENA.Mqtt.publish(`${publishTopicBase}/planes`, msg, undefined, undefined, true);
+                });
+            }
         }
     },
 });
