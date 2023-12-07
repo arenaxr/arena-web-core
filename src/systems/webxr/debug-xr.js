a8b83410508c60f47333ac447287063fd1a3109b
Ivan Liang <r33t@cmu.edu>
2023-12-07 16:27:50 -0500

    Defer debugXR bind until after params loaded

--- src/systems/webxr/debug-xr.js
+++ src/systems/webxr/debug-xr.js
@@ -4,28 +4,30 @@ import { ARENA_EVENTS } from '../../constants';
 
 AFRAME.registerSystem('debug-ui', {
     init() {
-        ARENA.debugXR = console.log;
+        ARENA.events.addEventListener(ARENA_EVENTS.ARENA_LOADED, this.setup.bind(this));
+    },
+    setup() {
         if (ARENA.params.debugUI) {
-            ARENA.events.addMultiEventListener([ARENA_EVENTS.ARENA_LOADED], () => {
-                const debugCard = document.createElement('a-entity');
-                debugCard.setAttribute('arenaui-card', {
-                    title: 'Debug',
-                    body: '',
-                    fontSize: 0.018,
-                    widthScale: '0.5',
-                });
-                debugCard.setAttribute('position', { x: 0, y: 0.2, z: -1 });
-                document.getElementById('my-camera').appendChild(debugCard);
-                ARENA.debugXR = (text, newline = true) => {
-                    const prevText = debugCard.getAttribute('arenaui-card').body;
-                    if (text === undefined) {
-                        debugCard.setAttribute('arenaui-card', { body: '' });
-                    } else {
-                        const n = newline ? '\n' : '';
-                        debugCard.setAttribute('arenaui-card', { body: `${prevText}${n}${text}` });
-                    }
-                };
+            const debugCard = document.createElement('a-entity');
+            debugCard.setAttribute('arenaui-card', {
+                title: 'Debug',
+                body: '',
+                fontSize: 0.018,
+                widthScale: '0.5',
             });
+            debugCard.setAttribute('position', { x: 0, y: 0.2, z: -1 });
+            document.getElementById('my-camera').appendChild(debugCard);
+            ARENA.debugXR = (text, newline = true) => {
+                const prevText = debugCard.getAttribute('arenaui-card').body;
+                if (text === undefined) {
+                    debugCard.setAttribute('arenaui-card', { body: '' });
+                } else {
+                    const n = newline ? '\n' : '';
+                    debugCard.setAttribute('arenaui-card', { body: `${prevText}${n}${text}` });
+                }
+            };
+        } else {
+            ARENA.debugXR = () => {};
         }
     },
 });
