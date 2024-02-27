730657ef66c5e65068dfa9786b4af839ad30d844
Ivan Liang <r33t@cmu.edu>
2024-02-27 14:57:35 -0500

    fix: bad raytrace xrselect after reloc in xr
    Binds xrselect raycaster to camera parent (spinner). When the
    xrselect *does* work in session, it's set on the sceneEl which
    never has a matrixWorld transform from identity. To still get
    rig offsets due to relocalization, but omit camera matrixWorld
    transforms, we attach to the cameraSpinner instead.

--- src/systems/webxr/webxr-device-manager.js
+++ src/systems/webxr/webxr-device-manager.js
@@ -26,19 +26,20 @@ AFRAME.registerComponent('webxr-device-manager', {
 
         this.isMobile = ARENAUtils.isMobile();
         this.mouseCursor = document.getElementById('mouse-cursor');
+        this.cameraSpinner = document.getElementById('cameraSpinner');
     },
 
     onWebXREnterVR() {
-        const { el, mouseCursor, isMobile, isWebXRViewer } = this;
+        const { el, mouseCursor, cameraSpinner, isMobile, isWebXRViewer } = this;
         const { sceneEl } = el;
         if (sceneEl.is('ar-mode')) {
             if (isMobile && !isWebXRViewer) {
                 mouseCursor.removeAttribute('cursor');
                 mouseCursor.removeAttribute('raycaster');
-                el.setAttribute('cursor', { rayOrigin: 'xrselect', fuse: false });
-                el.setAttribute('raycaster', { objects: '[click-listener],[click-listener-local]' });
-
-                el.components.cursor.onEnterVR(); // Manually trigger cursor callback
+                cameraSpinner.renderer = sceneEl.renderer; // Manually set renderer for cursor component (assumes sceneEl)
+                cameraSpinner.setAttribute('raycaster', { objects: '[click-listener],[click-listener-local]' });
+                cameraSpinner.setAttribute('cursor', { rayOrigin: 'xrselect', fuse: false });
+                cameraSpinner.components.cursor.onEnterVR(); // Manually trigger cursor callback for xr event binds
             }
             document.getElementById('env').setAttribute('visible', false);
             const arMarkerSys = sceneEl.systems.armarker;
@@ -47,11 +48,11 @@ AFRAME.registerComponent('webxr-device-manager', {
     },
 
     onWebXRRExitVR() {
-        const { el: sceneEl, mouseCursor, isMobile, isWebXRViewer } = this;
+        const { el: sceneEl, mouseCursor, cameraSpinner, isMobile, isWebXRViewer } = this;
         if (sceneEl.is('ar-mode')) {
             if (isMobile && !isWebXRViewer) {
-                sceneEl.removeAttribute('cursor');
-                sceneEl.removeAttribute('raycaster');
+                cameraSpinner.removeAttribute('cursor');
+                cameraSpinner.removeAttribute('raycaster');
                 mouseCursor.setAttribute('raycaster', { objects: '[click-listener],[click-listener-local]' });
                 mouseCursor.setAttribute('cursor', { rayOrigin: 'mouse' });
             }
