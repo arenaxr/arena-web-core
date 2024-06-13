a1ff384f5aace050b892f00f5b0c7bdff2816b19
Ivan Liang <r33t@cmu.edu>
2024-06-13 16:17:50 -0400

    Restore config "realm" into topic templates

--- src/constants/topics.js
+++ src/constants/topics.js
@@ -8,26 +8,16 @@
 
 /* global ARENA */
 
-/** *
+/**
  outputTopic
- defaults.realm
  FACE_TOPIC
  CLIENT_DISCONNECT_TOPIC_PREFIX
 
  # chat
-
  dstTopic
 
-
  _dbgTopic
-
-
-const toUid = ''; // TODO: src/systems/ui/chat.js:133
-const handName = ''; // TODO: src/components/camera/arena-hand.js:73
-const objectId = ''; // Template in object
-const { nameSpace, idTag, camName, sceneName, userName } = ARENA;
-
- ** */
+ */
 
 /**
  * ARENA pubsub topic variables
@@ -38,33 +28,35 @@ const { nameSpace, idTag, camName, sceneName, userName } = ARENA;
  * - camName - idTag prefixed with camera_ (e.g. camera_1448081341_jdoe)
  */
 
+const REALM = ARENA.defaults.realm;
+
 // prettier-ignore
 const TOPICS = Object.freeze({
     SUBSCRIBE: {
         NETWORK:               '$NETWORK',
-        CHAT_PUBLIC:           'realm/c/{nameSpace}/o/#',
-        CHAT_PRIVATE:          'realm/c/{nameSpace}/p/{idTag}/#',
-        DEVICE:                'realm/d/{userName}/#', // All client placeholder
-        PROC:                  'realm/proc/#',
-        SCENE_PUBLIC:          'realm/s/{nameSpace}/{sceneName}/+/+',
-        SCENE_PRIVATE:         'realm/s/{nameSpace}/{sceneName}/+/+/{camName}/#',
+        CHAT_PUBLIC:           `${REALM}/c/{nameSpace}/o/#`,
+        CHAT_PRIVATE:          `${REALM}/c/{nameSpace}/p/{idTag}/#`,
+        DEVICE:                `${REALM}/d/{userName}/#`, // All client placeholder
+        PROC:                  `${REALM}/proc/#`,
+        SCENE_PUBLIC:          `${REALM}/s/{nameSpace}/{sceneName}/+/+`,
+        SCENE_PRIVATE:         `${REALM}/s/{nameSpace}/{sceneName}/+/+/{camName}/#`,
     },
     PUBLISH: {
         NETWORK_LATENCY:       '$NETWORK/latency',
-        CHAT_PUBLIC:           'realm/c/{nameSpace}/o/{idTag}',
-        CHAT_PRIVATE:          'realm/c/{nameSpace}/p/{toUid}/{idTag}',
-        DEVICE:                'realm/d/{nameSpace}/{sceneName}/{idTag}',
-        SCENE_PRESENCE:        'realm/s/{nameSpace}/{sceneName}/x/{idTag}',
-        SCENE_USER:            'realm/s/{nameSpace}/{sceneName}/u/{userObj}',
-        SCENE_USER_PRIVATE:    'realm/s/{nameSpace}/{sceneName}/u/{toUid}/{userObj}',
-        SCENE_OBJECTS:         'realm/s/{nameSpace}/{sceneName}/o/{objectId}', // All client placeholder
-        SCENE_OBJECTS_PRIVATE: 'realm/s/{nameSpace}/{sceneName}/o/{toUid}/{camName}',
-        SCENE_RENDER:          'realm/s/{nameSpace}/{sceneName}/r/{camName}',
-        SCENE_RENDER_PRIVATE:  'realm/s/{nameSpace}/{sceneName}/r/{toUid}/{camName}',
-        SCENE_ENV:             'realm/s/{nameSpace}/{sceneName}/e/{camName}',
-        SCENE_ENV_PRIVATE:     'realm/s/{nameSpace}/{sceneName}/e/{toUid}/{camName}',
-        SCENE_PROGRAM:         'realm/s/{nameSpace}/{sceneName}/p/{camName}',
-        SCENE_PROGRAM_PRIVATE: 'realm/s/{nameSpace}/{sceneName}/p/{toUid}/{camName}',
+        CHAT_PUBLIC:           `${REALM}/c/{nameSpace}/o/{idTag}`,
+        CHAT_PRIVATE:          `${REALM}/c/{nameSpace}/p/{toUid}/{idTag}`,
+        DEVICE:                `${REALM}/d/{nameSpace}/{sceneName}/{idTag}`,
+        SCENE_PRESENCE:        `${REALM}/s/{nameSpace}/{sceneName}/x/{idTag}`,
+        SCENE_USER:            `${REALM}/s/{nameSpace}/{sceneName}/u/{userObj}`,
+        SCENE_USER_PRIVATE:    `${REALM}/s/{nameSpace}/{sceneName}/u/{toUid}/{userObj}`,
+        SCENE_OBJECTS:         `${REALM}/s/{nameSpace}/{sceneName}/o/{objectId}`, // All client placeholder
+        SCENE_OBJECTS_PRIVATE: `${REALM}/s/{nameSpace}/{sceneName}/o/{toUid}/{camName}`,
+        SCENE_RENDER:          `${REALM}/s/{nameSpace}/{sceneName}/r/{camName}`,
+        SCENE_RENDER_PRIVATE:  `${REALM}/s/{nameSpace}/{sceneName}/r/{toUid}/{camName}`,
+        SCENE_ENV:             `${REALM}/s/{nameSpace}/{sceneName}/e/{camName}`,
+        SCENE_ENV_PRIVATE:     `${REALM}/s/{nameSpace}/{sceneName}/e/{toUid}/{camName}`,
+        SCENE_PROGRAM:         `${REALM}/s/{nameSpace}/{sceneName}/p/{camName}`,
+        SCENE_PROGRAM_PRIVATE: `${REALM}/s/{nameSpace}/{sceneName}/p/{toUid}/{camName}`,
     },
 });
 
