951ea1050166541507cefd0633841465e2c0ab4a
Ivan Liang <r33t@cmu.edu>
2024-08-07 23:04:19 -0400

    Update device pub/sub topics

--- src/constants/topics.js
+++ src/constants/topics.js
@@ -47,7 +47,7 @@ const TOPICS = Object.freeze({
     },
     SUBSCRIBE: {
         NETWORK:               '$NETWORK',
-        DEVICE:                `${REALM}/d/{userName}/#`, // All client placeholder
+        DEVICE:                `${REALM}/d/{deviceName}/#`, // All client placeholder
         PROC_REG:              `${REALM}/proc/reg`,
         PROC_CTL:              `${REALM}/proc/control/{uuid}/#`,
         PROC_DBG:              `${REALM}/proc/debug/{uuid}`,
@@ -56,7 +56,7 @@ const TOPICS = Object.freeze({
     },
     PUBLISH: {
         NETWORK_LATENCY:       '$NETWORK/latency',
-        DEVICE:                `${REALM}/d/{nameSpace}/{sceneName}/{idTag}`,
+        DEVICE:                `${REALM}/d/{deviceName}/{idTag}`,
         PROC_REG:              `${REALM}/proc/reg`,
         PROC_CTL:              `${REALM}/proc/control`,
         PROC_DBG:              `${REALM}/proc/debug/{uuid}`,
