dc5430a32fdce96447b277d233b33babfb4953b1
mwfarb <mwfarb@andrew.cmu.edu>
2024-10-21 14:22:18 -0400

    test true update

--- src/components/object/ttl.js
+++ src/components/object/ttl.js
@@ -15,28 +15,24 @@ import { Delete } from '../../systems/core/message-actions';
  * Update *is* allowed, which will reset the timer to start from that moment. Note that this is a top-level property
  * in MQTT messages, with the seconds value simply as a scalar rather than a nested object property.
  *
- * @property {number} seconds - Seconds until entity is removed
+ * @property {int} expireAt - Epoch until entity is removed
  * @module ttl
  */
 AFRAME.registerComponent('ttl', {
     schema: {
-        seconds: { type: 'number' },
+        expireAt: { type: 'int' },
     },
     init() {
-        if (this.data.seconds > 0) {
+        console.log('ttl', 'init', this.data.expireAt, this.el.id);
+        if (this.data.expireAt > 0) {
             this.expireAt = Date.now() + this.data.seconds * 1000;
             this.tick = AFRAME.utils.throttleTick(this.tick, 1000, this);
         }
     },
-    update() {
-        if (this.data.seconds >= 0) {
-            // Allow -1 to bypass TTL update, retains previous timeout
-            this.expireAt = Date.now() + this.data.seconds * 1000;
-        }
-    },
     tick() {
         const now = Date.now();
-        if (this.expireAt && now > this.expireAt) {
+        if (this.data.expireAt > 0 && now > this.data.expireAt) {
+            console.log('ttl', 'delete', this.data.expireAt, this.el.id);
             Delete.handle({ id: this.el.id });
         }
     },
