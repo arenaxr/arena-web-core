6b7790bdb5a6f461c0acc2cf455a74d8b069b362
mwfarb <mwfarb@andrew.cmu.edu>
2025-01-28 09:27:37 -0500

    fix: user notification of relocalization in relocateUserCamera

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -463,7 +463,7 @@ export default class ARENAUtils {
                 }
             }
         }
-        AFRAME.scenes[0].systems['arena-chat-ui'].relocalizeMsg();
+        AFRAME.scenes[0].systems['arena-chat-ui'].relocalizedMsg();
     }
 
     /**
