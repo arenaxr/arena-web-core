8179e0895caaeda4efa553bb31959061fe925107
Ivan Liang <r33t@cmu.edu>
2025-03-29 16:35:27 -0400

    lint: autoformat vendor code

--- src/components/vendor/physx/force-pushable.js
+++ src/components/vendor/physx/force-pushable.js
@@ -11,56 +11,56 @@
  * Requires: physx
  */
 AFRAME.registerComponent('physx-force-pushable', {
-  schema: {
-    force: { default: 10 },
-    on: { default: 'mousedown' }
-  },
-  init: function () {
+    schema: {
+        force: { default: 10 },
+        on: { default: 'mousedown' }
+    },
+    init: function() {
 
-    this.pStart = new THREE.Vector3();
-    this.sourceEl = this.el.sceneEl.querySelector('[camera]');
-    this.forcePushPhysX = this.forcePushPhysX.bind(this);
+        this.pStart = new THREE.Vector3();
+        this.sourceEl = this.el.sceneEl.querySelector('[camera]');
+        this.forcePushPhysX = this.forcePushPhysX.bind(this);
 
-    this.sourcePosition = new THREE.Vector3();
-    this.force = new THREE.Vector3();
-    this.pos = new THREE.Vector3();
-  },
+        this.sourcePosition = new THREE.Vector3();
+        this.force = new THREE.Vector3();
+        this.pos = new THREE.Vector3();
+    },
 
-  play() {
-    this.el.addEventListener(this.data.on, this.forcePushPhysX);
-  },
+    play() {
+        this.el.addEventListener(this.data.on, this.forcePushPhysX);
+    },
 
-  pause() {
-    this.el.removeEventListener(this.data.on, this.forcePushPhysX);
-  },
+    pause() {
+        this.el.removeEventListener(this.data.on, this.forcePushPhysX);
+    },
 
-  forcePushPhysX: function (e) {
+    forcePushPhysX: function(e) {
 
-    const el = this.el
-    if (!el.components['physx-body']) return
-    const body = el.components['physx-body'].rigidBody
-    if (!body) return
+        const el = this.el;
+        if (!el.components['physx-body']) return;
+        const body = el.components['physx-body'].rigidBody;
+        if (!body) return;
 
-    const force = this.force
-    const source = this.sourcePosition
+        const force = this.force;
+        const source = this.sourcePosition;
 
-    // WebXR requires care getting camera position https://github.com/mrdoob/three.js/issues/18448
-    source.setFromMatrixPosition( this.sourceEl.object3D.matrixWorld );
+        // WebXR requires care getting camera position https://github.com/mrdoob/three.js/issues/18448
+        source.setFromMatrixPosition(this.sourceEl.object3D.matrixWorld);
 
-    el.object3D.getWorldPosition(force)
-    force.sub(source)
+        el.object3D.getWorldPosition(force);
+        force.sub(source);
 
-    force.normalize();
+        force.normalize();
 
-    // not sure about units, but force seems stronger with PhysX than Cannon, so scaling down
-    // by a factor of 5.
-    force.multiplyScalar(this.data.force / 5);
+        // not sure about units, but force seems stronger with PhysX than Cannon, so scaling down
+        // by a factor of 5.
+        force.multiplyScalar(this.data.force / 5);
 
-    // use data from intersection to determine point at which to apply impulse.
-    const pos = this.pos
-    pos.copy(e.detail.intersection.point)
-    el.object3D.worldToLocal(pos)
+        // use data from intersection to determine point at which to apply impulse.
+        const pos = this.pos;
+        pos.copy(e.detail.intersection.point);
+        el.object3D.worldToLocal(pos);
 
-    body.addImpulseAtLocalPos(force, pos);
-  }
+        body.addImpulseAtLocalPos(force, pos);
+    }
 });
