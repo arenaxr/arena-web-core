f839d66dfa238c35a28df52adcf31ae67d177727
mwfarb <mwfarb@andrew.cmu.edu>
2025-09-29 13:39:54 -0400

    fix(store): replace store iframe html injection with src update, improve error dialog

--- files/script.js
+++ files/script.js
@@ -5,41 +5,23 @@
 /* global ARENAAUTH $ */
 
 /**
- * Loads the html into the page iframe to completion.
- * @param {*} html The HTML code to apply into the iframe.
- */
-async function loadHtmlToFrame(html) {
-    const doc = document.getElementById('storeframe').contentWindow.document;
-    doc.open();
-    doc.write(html);
-    doc.close();
-
-    // Since filebrowser load will overwrite some style, after fs load, force any style we require
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'style.css';
-    document.getElementsByTagName('head')[0].appendChild(link);
-}
-
-/**
  * Connection to get the filestore proxy url served base html template into the iframe.
  * @param {*} authToken The auth token from filestore. 'None' when not authorized.
  */
 function loadStoreFront(authToken) {
+    const frame = document.getElementById('storeframe');
     try {
         // determine the token is formatted well
         ARENAAUTH.parseJwt(authToken);
         localStorage.setItem('jwt', authToken);
-        const xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-        xhr.open('GET', '/storemng');
-        xhr.send();
-        xhr.onload = () => {
-            if (xhr.status === 200) {
-                loadHtmlToFrame(xhr.response);
-            }
-        };
+        frame.setAttribute('src', '/storemng');
+
+        // Since filebrowser load will overwrite some style, after fs load, force any style we require
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'style.css';
+        document.getElementsByTagName('head')[0].appendChild(link);
     } catch (err) {
         console.warn(err);
         localStorage.setItem('jwt', null);
@@ -50,7 +32,14 @@ function loadStoreFront(authToken) {
                 if (window.auth.authenticated) {
                     fsErrorMsg = `Invalid file store auth token for user "${window.auth.username}".`;
                 }
-                loadHtmlToFrame(`<div style="text-align:center;">${fsErrorMsg}</div>`);
+                Swal.fire({
+                    icon: 'error',
+                    text: fsErrorMsg,
+                    allowEscapeKey: false,
+                    allowOutsideClick: false,
+                }).then((result) => {
+                    ARENAAUTH.signOut();
+                });
             }
         }, 250);
     }
