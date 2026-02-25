81b777435a52f86bc6ddcd06eabda91ab8909870
mwfarb <mwfarb@andrew.cmu.edu>
2026-02-25 00:52:43 -0500

    fix(multisrc): fix parsing error causing crash in multisrc

--- src/components/vendor/aframe-multisrc-component.js
+++ src/components/vendor/aframe-multisrc-component.js
@@ -1,18 +1,24 @@
 //aframe multisrc component
 
 AFRAME.registerComponent('multisrc', {
-  
+
   dependencies: ['material'],
-      
+
   schema: {
       srcs: {
           default: [],
           parse: function (value) {
-              if ( value.length == 0 ){
+              if ( !value || value.length == 0 ){
                 return ''
               }
-              else{
+              else if ( Array.isArray(value) ){
+                return value;
+              }
+              else if ( typeof value === 'string' ){
                 return value.split(',');
+              }
+              else{
+                return ''
               }
           }
       },
@@ -20,29 +26,29 @@ AFRAME.registerComponent('multisrc', {
   },//end schema
 
   init: function () {
-      
+
       //get the number of 'sides' (groups) of the shape this is attached to
       this.sides = this.el.getObject3D('mesh').geometry.groups.length
 
       //make new materials and add them to array
       this.makeMaterials();
-      
+
       //style the new materials by inheriting from default material component
       this.styleMaterials();
 
       //update the mesh with new materials array
       this.el.getObject3D('mesh').material = this.materials
-    
+
       //set up listeners for changes in default material
       this.materialListener();
-      
+
   },//end init
-  
+
   makeMaterials: function() {
-      
+
       //make an empty array to load some new materials into
       this.materials = []
-  
+
       //get the number of sides of the geometry make a separate material for each
       for(i=0;i<this.sides;i++){
 
@@ -50,29 +56,29 @@ AFRAME.registerComponent('multisrc', {
         this.materials.push( new THREE.MeshStandardMaterial({ name: 'material-' + i + ''}) )
 
       }//end for images loop
-  
+
   },//end makeMaterials
-  
+
   update: function (){
-    
+
       this.parseSrcs();
 
       this.addTextures();
-      
+
   },//end update
-  
+
   parseSrcs: function(){
-  
+
       //tidy up input from srcs
       // - video or image?, inline URL or id to asset?, all 'srcs' or 'src1' 'src2'? etc
       //make an array of objects with clean data
-      
+
       //make an empty array to store objects
       this.textures = []
-            
+
       //populate with objects for each
-      for (i=0;i<this.sides;i++){   
-        //choose attribute to use, 'srcs' or 'src1','src2' etc. can't use both 
+      for (i=0;i<this.sides;i++){
+        //choose attribute to use, 'srcs' or 'src1','src2' etc. can't use both
         var which;
         if ( this.data.srcs == '' ){
           which = this.data['src'+i]
@@ -94,12 +100,12 @@ AFRAME.registerComponent('multisrc', {
         this.textures[i].assetId = srcCheck(this.data, which, i).assetId
       }
 
-      function srcCheck(data, which, i){  
-        
+      function srcCheck(data, which, i){
+
         //inline or reference, image or video, 'srcs' attribute or 'src1' 'src2' etc.
 
         var src = which
-        
+
         //if one doesn't exist, don't break, just have empty values
         if (typeof src === 'undefined' || src == ''){
           url = ''
@@ -107,9 +113,9 @@ AFRAME.registerComponent('multisrc', {
           inline = ''
           assetId = ''
         }
-        
+
         else{
-          
+
           var asset;
           var url;
           var type;
@@ -117,7 +123,7 @@ AFRAME.registerComponent('multisrc', {
           var assetId;
 
           //if its not a preloaded asset, its an inline url so get that
-          if ( document.getElementById( src.replace('#','') ) === null ){ 
+          if ( document.getElementById( src.replace('#','') ) === null ){
             asset = src
             inline = true
           }
@@ -132,7 +138,7 @@ AFRAME.registerComponent('multisrc', {
 
           //check type, image or video
           //check if its an image or video
-          if ( url.includes('.jpg') || url.includes('.jpeg') || url.includes('.png') || url.includes('.gif') ){ 
+          if ( url.includes('.jpg') || url.includes('.jpeg') || url.includes('.png') || url.includes('.gif') ){
             type = 'image'
           }
           else{
@@ -146,28 +152,28 @@ AFRAME.registerComponent('multisrc', {
           inline: inline,
           assetId: assetId,
         }//end return
-        
+
       }//end srcCheck
-    
+
   },//end parseSrcs
-  
+
   addTextures: function(){
-      
+
       //loop through all textures and apply as defined
       for(i=0;i<this.textures.length;i++){
-        
+
           //each texture
           var texture = this.textures[i]
-          
+
           //get each material to update map
           var material = this.materials[i]
 
           //if the texture has no image/video assigned, undo any changes that have been made
           if ( texture.url == '' ){
-            
+
             material.map = null
             material.needsUpdate = true;
-            
+
           }
 
           //else update the material map with image/video
@@ -187,25 +193,25 @@ AFRAME.registerComponent('multisrc', {
 
             //else its a video, need to do a bit more
             else{
-              
+
               var video;
               //if its an inline src
-              if ( texture.srcInline ){ 
+              if ( texture.srcInline ){
                 video = document.createElement('video')
                 video.src = texture.url
                 video.crossOrigin = 'anonymous';
                 video.loop = true;
                 video.preload = 'auto';
                 video.load();
-                video.play(); 
+                video.play();
               }
-              //otherwise its an asset 
+              //otherwise its an asset
               else{
                 video = document.getElementById( texture.assetId.replace('#','') )
                 video.src = texture.url
                 video.load();
               }//end else its an asset
-              
+
               //in any case..
               var videoTexture = new THREE.VideoTexture(video);
               videoTexture.needsUpdate;
@@ -216,24 +222,24 @@ AFRAME.registerComponent('multisrc', {
               material.needsUpdate = true;
 
             }//end it its a video
-            
+
           }//end else texture has video/image assigned
-        
+
       }//end for textures
-    
+
   },//end addTextures
-  
+
   materialListener: function() {
-      
+
       //to listen for changes in components attached to parent entity
       //Note: default components, position, scale, rotation, visible, do not fire componentchanged unless they are specified as attributes on the entity
       //not an issue here as we are only looking for changes to material which always seems to fire, maybe because we have a dependency?
-      
+
       var self = this //can't get this inside eventListener
-      
+
       //if componentchange is for material, run styleMaterials
       this.compChange = function (evt) {
-        
+
           //if the material is being changed by an animation, reduce throttle to keep up with changes and styleMaterials
           if ( evt.detail.name == 'material' && this.isAnimating ){
             self.reduceMaterialChangedThrottle(0);
@@ -246,22 +252,22 @@ AFRAME.registerComponent('multisrc', {
           else{
             //may not always fire, see above
           }
-         
+
       }//end compChange
-      
+
       //check for component changes and then run the above compchange function
       this.el.addEventListener('componentchanged', this.compChange);
-    
+
       //handling changes to animation, need to dethrotle componentchange event to keep up, but put back when animation is over
-    
+
       //store a default state
       this.isAnimating = false
       //keep a count of how many are going on at once, to handle events on multiple animations
       this.animationCount = 0
-    
+
       //add another listener here for animationbegin
       this.materialChangedByAnimation = function (evt) {
-        
+
         //if an animation has begun set isAnimating to true so that dethrottle can then be activated in componentchanged, also add an animation to animationCount
         if ( evt.type == 'animationbegin' || evt.type == 'animationstart' ){
           this.isAnimating = true
@@ -282,41 +288,41 @@ AFRAME.registerComponent('multisrc', {
           if ( evt.type == 'animationcomplete' ){
             self.el.components[evt.detail.name].animation.reset();
           }
-          
+
         }
-        
+
       }
-      
-      //make an array of animation events 
+
+      //make an array of animation events
       //a mix of animation component events and <a-animation> events to handle both
       var animationEventsArray = ['animationbegin', 'animationstart', 'animationcomplete', 'animationend']
-      
+
       //attach them all to listen for each
       animationEventsArray.forEach(function(event) {
         self.el.addEventListener(event, self.materialChangedByAnimation)
       });
-    
+
   },//end materialListener
-  
+
   styleMaterials: function(){
-    
+
       //to inherit changes from material component
-    
+
       //get attached material component styles to copy
-      var styles = this.el.components.material.material  
-      
+      var styles = this.el.components.material.material
+
       //get number of sides and loop through them
       for(i=0;i<this.sides;i++){
-    
+
         //get each material
         var material = this.materials[i]
-        
+
           //get the difference between default material and our material
           var a = material
           var b = styles
           var diff = AFRAME.utils.diff (a, b)
 
-            //use the different styles to style the materials unless it is the uuid, map or name 
+            //use the different styles to style the materials unless it is the uuid, map or name
             for (var key in diff) {
                 if (material.hasOwnProperty(key)) {
 
@@ -330,65 +336,66 @@ AFRAME.registerComponent('multisrc', {
 
                 }//end if has own property
             }//end for keys in data
-        
+
       }//end for images loop
-  
+
   },//end styleMaterials
-  
+
   granularChange: function (materialIndex) {
-      
+
       //get and return a specific material/side for styling individually - advanced
       var material = this.el.getObject3D('mesh').material[materialIndex]
       return material
-    
+
   },//end granularChange
-  
+
   reduceMaterialChangedThrottle: function(throttle){
-  
+
       //change throttle on material componentchanged so as to update quickly enough for animations, from 200 to 0
       var material = this.el.components.material
-      
-      material.throttledEmitComponentChanged = AFRAME.utils.throttle(function emitChange () { 
-        material.el.emit('componentchanged', material.evtDetail, false); 
+
+      material.throttledEmitComponentChanged = AFRAME.utils.throttle(function emitChange () {
+        material.el.emit('componentchanged', material.evtDetail, false);
       }, throttle);
 
   },//end reduceMaterialChangedThrottle:
-  
+
   updateSchema: function () {
-        
+
         //update schema with an attribute for each individual side of attached geometry
-    
-        //have to get the sides afresh as this is run before init
-        var sides = this.el.getObject3D('mesh').geometry.groups.length  
+
+        var mesh = this.el.getObject3D('mesh');
+        if (!mesh || !mesh.geometry || !mesh.geometry.groups) return;
+        var sides = mesh.geometry.groups.length
         var newSchema = {}
         for (i = 0; i < sides; i++){
             newSchema["src"+i] = {type: 'string', default: ''};
         }
         this.extendSchema(newSchema);
   },//end updateSchema
-  
+
   remove: function(){
-    
+
     //removes this multimaterial from mesh, adds the default material back on, removes event listeners and puts material componentchanged throttle back to normal if necessary
-    
+
     //get the default material
-    var defaultMaterial = this.el.components.material.material 
-    
+    var defaultMaterial = this.el.components.material.material
+
     //update the mesh with default material
     this.el.getObject3D('mesh').material = defaultMaterial
-    
+
     //remove componentchanged eventlistener
     this.el.removeEventListener('componentchanged', this.compChange);
-    
+
     //removes all animation event listeners at once
     var animationEventsArray = ['animationbegin', 'animationstart', 'animationcomplete', 'animationend']
     animationEventsArray.forEach(function(event) {
       this.el.addEventListener(event, this.materialChangedByAnimation)
     });
-    
+
     //put component changed throttle back to normal (in case it was removed while animating)
     this.reduceMaterialChangedThrottle(200);
-    
+
   },//end remove
 
 });//end multisrc
